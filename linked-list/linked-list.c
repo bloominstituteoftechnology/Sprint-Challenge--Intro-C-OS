205230ae574e09944e747ac583bf249c276d2585
Donnie Reese <donniereese@gmail.com>
2017-12-15 13:36:46 -0600

    That was an experience!  Finished the linked list and completed the written assignment.

--- linked-list/linked-list.c
+++ linked-list/linked-list.c
@@ -1,6 +1,47 @@
-new_node()
+#include <stdio.h>
+#include <stdlib.h>
 
-append_node()
+struct node {
+    struct node *head; // `struct node* rabbithole`
+    struct node *next; // `struct node* ladder`
+    int payload; // `char distraction`
+};
+
+struct node* new_node(struct node *head, int payload)
+{
+    struct node* newNode = malloc(sizeof(struct node));
+
+    newNode->head = head;
+    newNode->next = NULL;
+    newNode->payload = payload;
+    
+    return newNode;
+}
+
+void append_node(struct node *n, struct node *nn)
+{
+    struct node *curHead = n;
+    // loop through next of curHead until next is null
+    while (curHead->next != NULL) {
+        curHead = curHead->next;
+    }
+    curHead->next = nn;
+}
+
+void print_values(struct node *head)
+{
+    while (head != NULL) {
+        printf("valueL %d \n", head->payload);
+        head = head->next;
+        
+    }
+}
 
 int main(void) {
+    struct node *head = new_node(NULL, 4);
+    append_node(head, new_node(head, 3));
+
+    print_values(head);
+    
+    return 0;
 }
