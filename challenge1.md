2c684b2ccb246d0e0d93860b62c0b5e5b6e45818
kairat abylkasymov <kairat.ninety@gmail.com>
2017-12-15 15:06:02 -0500

    SC

--- challenge1.md
+++ challenge1.md
@@ -1,1 +1,7 @@
 Answer here
+
+When it comes to boot order, CPU gets power and loads BIOS (collection of programs contained in the Read Only Memory (ROM) chip). ROM allows the CPU to communicate with various devices attached to the motherboard The first BIOS process that occurs is Power On Self Test (POST). POST allows us to read and write a memory, send commands to CPU. 
+
+So I guess that would probably enough to manage system and write a program if we know C lanaguage. Why C language? Most of operating systems were written in C or C++. 
+
+But I'am not sure if could open open Comand promt(cmd) or terminal in BIOS mode. :)
--- linked-list/linked-list.c
+++ linked-list/linked-list.c
@@ -1,6 +1,92 @@
-new_node()
+#include <stdio.h>
+#include <stdlib.h>
 
-append_node()
+struct node {
+    int data;
+    struct node* next;
+};
 
-int main(void) {
+struct node* head;
+
+struct node* new_node(int data) {
+
+    struct node* newNode = malloc(sizeof(struct node));
+
+    newNode->next = NULL;
+    newNode->data = data;
+
+    return newNode;
+}
+
+void append_node(struct node* list, struct node* node)
+{
+    while (list->next != NULL) {
+        list = list->next;
+    }
+
+    list->next = node;
+}
+
+struct node* search(struct node* list, int data) {
+
+    while (list != NULL) {
+        if (list->data == data) {
+            return list;
+        }
+        list = list->next;
+    }
+    return NULL;
+}
+
+void delete(struct node* list, int data) {
+    
+    struct node* iterator = list;
+
+    while (iterator != NULL) {
+        if (iterator->next->data == data) {
+            printf("success node with provided data removed %d\n", data);
+            break;
+        }
+        iterator = iterator->next;
+    }
+
+    if (iterator != NULL) {
+        struct node* tmp = iterator->next;
+        iterator->next = tmp->next;
+        tmp->next = NULL;
+        free(tmp);
+    }
+
+}
+
+
+int main(void) 
+{
+
+    struct node* tail = new_node(5);
+    struct node* newNode = new_node(6);
+    struct node* head = tail;
+
+    append_node(tail, newNode);
+
+    struct node* node = new_node(10);
+
+    append_node(tail, node);
+
+    delete(tail, 6);
+
+    struct node* result = search(tail, 10);
+    if (result == 0) {
+        printf("unsuccess node was not found %d\n", result);
+    } else {
+        printf("success node found %d\n", result->data);
+    }
+    
+    while (head->next != NULL) {
+        printf("%d\n", head->data);
+        head = head->next;
+    }
+
+    return 0;
+
 }
