dd1ef5285e6ffb9b6f2c73f6e33d6f7800977b6a
Collin Ferguson <theredcouchroom@gmail.com>
2017-12-15 12:45:00 -0600

    Linked list adds nodes, appends them, and can search.

--- challenge1.md
+++ challenge1.md
@@ -1,1 +1,9 @@
 Answer here
+
+If no opporating system was in place, writing software would be much more diffucult.
+The software would have to control writing files and keeping track of their addresses.
+The software would also have to communicate with drivers specifically to do things like control the screen, access the hard drive, access graphics cards,
+manage RAM, and do all of those in a way that's specialized for each individual part.
+You would have to write all of this in assembly language which I know almost nothing about.
+Sounds pretty bad.
+Massive steps would have to be taken to perform the simplist of tasks.
--- linked-list/linked-list.c
+++ linked-list/linked-list.c
@@ -1,6 +1,53 @@
-new_node()
+#include <stdlib.h>
+#include <stdio.h>
 
-append_node()
+typedef struct node {
+    int *value;
+    struct node *next;
+} node;
+
+node *new_node(int *x)
+{
+    node n;
+    node *nd = malloc(sizeof(n));
+    nd->value = x;
+    nd->next = NULL;
+    return nd;
+}
+
+int *append_node(node *n, node *d)
+{
+    n->next = d;
+    return 0;
+}
+
+int search(int *val, node *head_node)
+{
+    node *m = head_node;
+    int *z = val;
+    while (&*(m)->value != z || &*(m)->next != NULL)
+    {
+        m = &*(m)->next;
+    }
+    if (&*(m)->value == z)
+    {
+        return 1;
+    }
+    return 0;
+}
 
 int main(void) {
+    int val1 = 3;
+    int val2 = 5;
+    node *nd1 = new_node(&val1);
+    node *nd2 = new_node(&val2);
+    append_node(nd1, nd2);
+    node *y = &*(nd1)->next;
+    int *z = &*(y)->value;
+    printf("The next node's value is %d\n", *(z));
+    int found = search(&val2, nd1);
+    printf("The value 5 found? %d\n", found);
+    free(nd1);
+    free(nd2);
+    return 0;
 }
