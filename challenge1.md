72beea8e32e44bf79abe14fa331033159aa84264
ceylonsullen <hussainaljahmi@gmail.com>
2017-12-15 14:52:08 -0500

    could not push through all bugs

--- challenge1.md
+++ challenge1.md
@@ -1,1 +1,18 @@
 Answer here
+
+#Run a Program without an Operating System
+
+## Commands that utilize computer's logic gates  
+We would require some instructions for the computer that utilize the logic gates inside the CPU to produce the  
+desired results. In order to compute efficiently, we would need to store memory in the RAM, so we would have to have  
+some way of storing to the ram some registers that hold values that are going to be used in computing or the  
+result of these computations. 
+
+Then, utilizing commands, and storing values to registers, we can access registers  
+and compute values. If we require the computer to do anything else such as displaying data, it would entail  
+giving instructions to the computer specific to the hardware so that it could display a value to the screen.  
+
+
+If we wanted to save data to the hard disk, we would have to give the computer commands according to the hardware   
+of the hard disk, commands that include storing binary representations of data in the sections of the hard disk  
+and a method of retrieving, overwriting, or deleting the data.
--- linked-list/linked-list.c
+++ linked-list/linked-list.c
@@ -1,6 +1,63 @@
-new_node()
 
-append_node()
+#include <stdio.h>
+
+#include <stdlib.h>
+
+struct node 
+{
+    int value;
+    struct node *next;
+};
+struct list {
+    struct node *head;
+};
+
+struct node* new_node() {
+    struct node *newNode = malloc(sizeof(struct node));
+    puts("hi");
+    newNode-> next = malloc(sizeof(struct node));
+    puts("hi");
+    newNode-> next = NULL;
+    newNode-> value = 0;
+    puts("hi");
+    return new_node;
+};
+
+int append_node(struct node *list_head, int x) {
+    struct node *current;
+    puts("oh");
+    *current = *list_head;
+    puts("oh");
+    while(list_head -> next != NULL) {
+        current = current -> next;
+    puts("oh");
+    }
+    puts("oh");
+    current -> next = new_node();
+    puts("oh");
+    current = current -> next;
+    puts("oh");
+    current -> value = x;
+    puts("oh");
+    return 0;
+}
 
 int main(void) {
+    
+    
+    puts("hi");
+    struct node *nodeA;
+    puts("nodeA made");
+    nodeA = new_node();
+    
+    printf("%d", 3);
+    puts("nodeA got malloced");
+    struct node nodeB;
+    nodeB = *nodeA;
+    int r = nodeB.value;
+    printf("%d \n", r);
+    append_node(nodeA, 3);
+    printf("%d \n", 3);
+//nodeA -> value
+
 }
