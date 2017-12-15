16f8bd5eb280d5a5246a6e97d3614a83d3d76f79
Neil Barduson <barduson.neil@gmail.com>
2017-12-15 13:14:22 -0600

    I never actually go to the Operating-Systems One, so this was my first attempt.  I feel some what comfortable with all the concepts I wrote in my answer.

--- challenge1.md
+++ challenge1.md
@@ -1,1 +1,23 @@
-Answer here
+## challenge 1:
+
+Which means you would have to create your own bare bones mini OS?
+If thats the case then:
+
+a boat loader, to load your mini OS.
+You would need to write logic with a low level lang (C or ASM) for:
+	Memory Managment,
+	File directoy system,
+	and Drivers for periphals
+
+	As well a kernel that handles:
+		threads, processes, and scheduler to handle the threads and processes
+
+I should go deeper into and describe the inner workings and mechanicms of those concepets.
+As that would add to the answer of the question.
+
+To get to the question of "what would you need to do to write software with no OS",
+is it possible to right software with no OS?  Maybe basic a calculator or funcationalities
+that hinge on basic arthmatic, like a clock or a score board?  But if you were to do anything remotely
+complex, the amount of unreadable code would grow exponentially.
+
+So it seems you need an OS to handle the lowest level of abstractions in memory and arithmatic.
--- linked-list/linked-list.c
+++ linked-list/linked-list.c
@@ -1,6 +1,90 @@
-new_node()
+#include <stdio.h>
+#include <stdlib.h>
 
-append_node()
+struct ll {
+	int val;
+	struct ll* next;
+} ll;
+
+struct ll* new_node(int val)
+{
+	// allocate memory
+	struct ll *node = malloc(sizeof(struct ll));
+
+	node->val = val;
+	node->next = NULL;
+
+	return node;
+}
+
+void append_node(struct ll* list, int val)
+{
+	// go to end of list
+	while (list->next != NULL) {
+		list = list->next;
+	}
+
+	// instantiate newNode
+	struct ll* newNode = new_node(val);
+
+	// add newNode to end of the list
+	list->next = newNode;
+}
+
+// searches for node in linked list, if found, returns the index
+int search_node(struct ll* list, int searchVal)
+{
+	int index = 0;
+	while (list->next != NULL) {
+		if (list->val == searchVal) {
+			return index;
+			// break;
+		}
+		index++;
+		list = list->next;
+	}
+	return -1;
+}
+
+void delete_node(struct ll** head_ref, int searchVal)
+{
+	// check if deleted node is the first node
+	// if its in the middle, point the nodes previous to the nodes next
+	// do we delete and free the memory of the nodes memory?
+
+	struct ll* tmp = *head_ref, *prev;
+
+	// If head node is the prospected deleted node
+	if (tmp != NULL && tmp->val == searchVal)
+	{
+		// point head to the next node, essentially removing the head.
+		*head_ref = tmp->next;
+		// free the memory used by the head
+		free(tmp);
+		// return the linked list
+		return;
+	}
+
+	// Search for the searchVal to be deleted, keep track of the
+	// previous node as we need to change 'prev->next'
+	while (tmp != NULL && tmp->val != searchVal)
+	{
+		// keep track of the previous node
+		prev = tmp;
+		// point the current tmp to the next node
+		tmp = tmp->next;
+	}
+
+	// if searchVal wasnt found, return the linked list
+	if (tmp == NULL) return;
+
+	// Unlink the node from linked list
+	prev->next = tmp->next;
+
+	// free the memory of the unlinked node
+	free(tmp);
+}
 
 int main(void) {
+	return 0;
 }
