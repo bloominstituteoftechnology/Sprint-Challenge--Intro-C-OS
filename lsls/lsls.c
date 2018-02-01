03cd74fe9cc598ac1c9005e777b0ae39ae1c9edb
MannieJay <jhall02258@gmail.com>
2018-02-01 16:47:06 -0500

    challenge 2 completed (stretch incomplete debugging needed)

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,59 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <unistd.h>
+#include <sys/stat.h>
+char *getcwd(char *buf, size_t size);
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  struct stat *file_stats;
+  DIR *pDir;
+  struct dirent *pDirent;
+  char cwd[1024];
 
-  // Open directory
+  if (argc < 2)
+  {
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+      pDir = opendir(cwd);
+      while ((pDirent = readdir(pDir)) != NULL)
+      {
+        printf("[%s]\n", pDirent->d_name);
+      }
+      closedir(pDir);
+      return 0;
+    }
+  }
+  else if (argc >= 2)
+  {
+    pDir = opendir(argv[1]);
+  }
+  if (pDir == NULL)
+  {
+    printf("Cannot open directory '%s'\n", argv[1]);
+    return 1;
+  }
 
-  // Repeatly read and print entries
+  do
+  {
+    pDirent = readdir(pDir);
+    if (pDirent)
+    {
+      printf("%s -- ", pDirent->d_name);
+      if (!stat(pDirent->d_name, file_stats))
+      {
+        printf("%u bytes\n", (unsigned int)file_stats.st_size);
+      }
+      else
+      {
+        printf("(size unavailable for this file)\n");
+      }
+    }
 
-  // Close directory
-
+  } while (pDirent);
+  closedir(pDir);
   return 0;
 }
