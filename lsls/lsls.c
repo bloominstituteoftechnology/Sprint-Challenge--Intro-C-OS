f20e2a5ba0d7d136e3029039330666ffc00f69ee
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-02-02 11:27:01 -0700

    Challenge 2 completed

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,27 @@
  */
 int main(int argc, char **argv)
 {
+  struct dirent *pDirent;
+  DIR *pDir;
+
   // Parse command line
+  if (argc < 2) {
+    argv[argc++] = ".";
+  }
 
   // Open directory
+  pDir = opendir(argv[1]);
+  if (pDir == NULL) {
+    printf("Cannot open directory '%s'\n", argv[1]);
+    return 1;
+  }
 
   // Repeatly read and print entries
-
+  while ((pDirent = readdir(pDir)) != NULL) {
+      printf ("%s\n", pDirent->d_name);
+  }
   // Close directory
+  closedir(pDir);
 
   return 0;
 }
