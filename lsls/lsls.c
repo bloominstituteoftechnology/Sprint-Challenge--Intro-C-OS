b1d93a6a2570ac3d0e27ed52be1638eea7fdc795
Teresa Strout <qapla47@gmail.com>
2018-02-02 14:18:09 -0500

    answers done

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -4,8 +4,30 @@
 /**
  * Main
  */
-int main(int argc, char **argv)
-{
+
+// this parses the number of args passed in:
+// lsls.exe returns "The 0 arg is: lsls.exe"
+// lsls.exe . returns "The 0 arg is: lsls.exe \ The 1 arg is: ."
+// argc is arg counter, argv is arg value
+//int main(int argc, char **argv)
+int main(void)
+// {
+
+//   for (int i = 0; i < argc; i++) {
+//     printf("The %2d arg is: %s\n", i, argv[i]);
+//   }
+
+struct dirent *de;
+DIR *dr = opendir(".");
+
+if (dr == NULL) {
+  printf("Could not open current directory");
+  return 0;
+}
+while ((de = readdir(dr)) != NULL)
+  printf("%s\n", de->d_name);
+  closedir(dr);
+
   // Parse command line
 
   // Open directory
