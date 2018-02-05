d28b54c304df418ea5fa167d270bcb364279801c
Christy Crites <cacrites2007@gmail.com>
2018-02-05 09:05:08 -0500

    Stretch goal 1 finished

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
-/**
- * Main
- */
 int main(int argc, char **argv)
 {
   // Parse command line
-
   // Open directory
-
   // Repeatly read and print entries
-
   // Close directory
+  int len;
+  struct dirent *pDirent;
+  DIR *pDir;
 
+  if (argc < 2) 
+  {
+    pDir = opendir(".");
+  }
+
+  else pDir = opendir (argv[1]);
+  
+  if (pDir == NULL) 
+  {
+    printf ("Cannot open directory '%s'\n", argv[1]);
+    return 1;
+  }
+
+  while ((pDirent = readdir(pDir)) != NULL) 
+  {
+    printf ("%10lld %s\n", 1111111111, pDirent->d_name);
+  }
+  closedir (pDir);
   return 0;
 }
+
