43da1f018a21aa73b6f04dcf8ac3351ab7019cbb
William Antony <william@pathadan.com>
2018-03-08 17:16:10 -0600

    Completed Stretch

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -7,12 +8,51 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  if (argc > 1)
+  {
+    char *path = argv[1];
 
-  // Open directory
+    // Open directory
+    DIR *directory = opendir(path);
 
-  // Repeatly read and print entries
+    if (directory != '\0')
+    {
+      // Repeatly read and print entries
+      struct dirent *entry;
 
-  // Close directory
+      while ((entry = readdir(directory)) != '\0')
+      {
+        char printString[1024];
+        char *filename = entry->d_name;
+
+        struct stat buf;
+
+        stat(filename, &buf);
+
+        int filesize = buf.st_size;
+
+        if ((buf.st_mode & S_IFDIR) != 0) {
+          // Directory
+          sprintf(printString, "%10s  %s\n", "<DIR>", filename);
+        }
+        else if ((buf.st_mode & S_IFREG) != 0) {
+          // File
+          sprintf(printString, "%10d  %s\n", filesize, filename);
+        }
+        else {
+          // Something else like device-node, symbolic link etc.
+          sprintf(printString, "---");
+        }
+
+        printf("%s", printString);
+
+      }
+
+      // Close directory
+      closedir(directory);
+    }
+
+  }
 
   return 0;
 }
