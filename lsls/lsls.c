5f072aa3851549906e274d635bf94f0caef27e2b
Sneha Thadani <snehacopper@gmail.com>
2018-03-09 08:28:46 -0800

    Finished Challenge 1

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -4,8 +4,26 @@
 /**
  * Main
  */
+
+void printEntries(char* dirname) {
+	DIR* handle = opendir(dirname);
+	struct dirent* entry = NULL;
+	do {
+		entry = readdir(handle);
+		if(entry == NULL)
+			break;
+		printf("%s\n", entry->d_name);
+	} while(entry != NULL);
+	closedir(handle);
+}
+
 int main(int argc, char **argv)
 {
+	if(argc == 1)
+		printEntries(".");
+	else
+		printEntries(argv[1]);
+
   // Parse command line
 
   // Open directory
