3b2b7df77e543a402aa83ef5fe3f34e26bb778e2
Cole Phillips <nphillips78@gmail.com>
2018-03-09 12:29:10 -0500

    just because

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -9,10 +9,22 @@ int main(int argc, char **argv)
   // Parse command line
 
   // Open directory
+  DIR *directory;
+  directory = opendir(directory);
+
+  if (directory == NULL)
+  {
+    printf("haha nope");
+  }
 
   // Repeatly read and print entries
+  if ( *directory !== '\0')
+  {
+    
+  }
 
   // Close directory
+  closedir(directory);
 
   return 0;
 }
