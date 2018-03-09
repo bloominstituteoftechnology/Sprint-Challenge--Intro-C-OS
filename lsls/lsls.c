c6bdb3591b5839f2ed1de500985e4d3ba27a5bc1
Jamie Domingo <jaybmw@aol.com>
2018-03-09 10:32:58 -0800

    attempted challenge

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
 #include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * Main
@@ -7,12 +8,34 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  int i;
+
+  printf("There are %d command line argument(s):\n", argc);
+
+  for (i = 0; i < argc; i++)
+  {
+    printf("   %s\n", argv[i]);
+  }
+
+  return 0;
+
+  struct dirent *pDirent;
+  DIR *pDir;
 
   // Open directory
+  if ((pDir = opendir("argv")) == NULL)
+  {
+    printf("Cannot open '%s'\n", argv[i]);
+    exit(0);
+  }
 
   // Repeatly read and print entries
+  while ((pDirent = readdir(pDir)) != NULL)
+  {
+    printf("[%s]\n", pDirent->d_name);
+  }
 
   // Close directory
-
+  closedir(pDir);
   return 0;
 }
