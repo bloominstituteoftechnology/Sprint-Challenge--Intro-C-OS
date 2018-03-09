49f5b7c0b96da0c6b88e4f719e0a6594cddc0c74
Ting Wang <felix.wang.1026@gmail.com>
2018-03-09 12:19:16 -0500

    print size of the files

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 int printDir(char *path)
 {
   DIR *d;
   struct dirent *dir;
+  struct stat buff;
+  char file_path[128];
+
   d = opendir(path);
   if (d)
   {
     while ((dir = readdir(d)) != NULL)
     {
-      printf("%s\n", dir->d_name);
+      strcat(file_path, path);
+      strcat(file_path, "/");
+      strcat(file_path, dir->d_name);
+      stat(file_path, &buff);
+      printf("%lld %s\n", buff.st_size, dir->d_name);
+      file_path[0] = '\0';
     }
     closedir(d);
   }
