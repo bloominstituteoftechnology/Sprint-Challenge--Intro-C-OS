c03c7b8738e804708b10275f1fe86048f5b34d3e
Pat Collins <padjo@mac.com>
2018-03-09 17:54:28 +0000

    dir listing working, questions answered

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,28 @@
  */
 int main(int argc, char **argv)
 {
+  char *dir;
+  struct dirent *de;
   // Parse command line
-
+  if (argc > 1) { // get the dir the user inputted
+    dir = argv[1];
+  } else {
+     dir = ".";
+  }
   // Open directory
-
+  // opendir() returns a pointer of DIR type. 
+    DIR *dr = opendir(dir); 
+ 
+    if (dr == NULL)  // opendir returns NULL if couldn't open directory
+    {
+        printf("Could not open current directory" );
+        return 0;
+    }
   // Repeatly read and print entries
-
+  while ((de = readdir(dr)) != NULL)
+            printf("%s\n", de->d_name);
+ 
   // Close directory
-
+  closedir(dr); 
   return 0;
 }
