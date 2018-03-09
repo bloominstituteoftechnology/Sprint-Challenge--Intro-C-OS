5f4450ba495b291aa58c0f85e77bdf98649ccfd4
cmoore4os <cm4glos@gmail.com>
2018-03-09 14:39:28 -0500

    Consuella -- completed

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,34 @@
  */
 int main(int argc, char **argv)
 {
+	DIR *directory;
   // Parse command line
+  if (argc == 2) {
+	  directory = argc[1];
+	}
+	else {
+		printf("Usage: <directory>/n args - name of directory to lookup";
 
   // Open directory
 
+  opendir(directory)
+   if (d == NULL)
+  {
+    printf("Error: Could not read directory\n");
+    exit(ENOENT);
+  }
   // Repeatly read and print entries
+  struct dirent *currentDir = readdir(directory);
 
-  // Close directory
+  while(currentDir != NULL)
+  {
+    printf("%s\n", currentDir->dir_name);
+    currentDir = readdir(directory);
+  }
+   // Close directory
+  closedir(directory);
 
+  return 0;
+}
   return 0;
 }
