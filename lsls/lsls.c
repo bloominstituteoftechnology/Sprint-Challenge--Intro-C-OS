3367c8c936adf55c4be6aab859f40ad1868e8fd8
Moises Dobarganes <designerexpert@gmail.com>
2018-03-09 13:20:52 -0500

    Finished with 2 Stretch Goals Done

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -35,18 +35,18 @@ int main(int argc, char **argv)
     // within the currentDirectory
     // print a new line with it.
     {
-      struct stat buf;
-      char type[1024];
-      stat(entry->d_name, &buf);
-      if (entry->d_type == DT_DIR)
+      struct stat buf;             // Contains each item's system stats.
+      char type[1024];             // Will contain File size or <DIR>.
+      stat(entry->d_name, &buf);   // Extracts stats into buf.
+      if (entry->d_type == DT_DIR) // Checks to see if it is a directory.
       {
-        sprintf(type, "%s", "<DIR>");
+        sprintf(type, "%s", "<DIR>"); // assigns <DIR> to the type char array.
       }
-      else
+      else // In the case it is not a directory
       {
-        sprintf(type, "%ld", buf.st_size);
+        sprintf(type, "%ld", buf.st_size); // assigns the size to the type char array.
       }
-      printf("  %6s  %s\n", type, entry->d_name);
+      printf("  %6s  %s\n", type, entry->d_name); // Prints out a line containing type/size and name of the item.
     }
     closedir(dir);
   }
