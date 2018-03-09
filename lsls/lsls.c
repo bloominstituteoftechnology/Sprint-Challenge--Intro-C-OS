79ae419b1da8b069df61795a79f0a8e95f895720
Ellen Nitchals <ellen.nitchals@gmail.com>
2018-03-09 10:46:33 -0800

    give it a try.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,30 @@
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  // Parse command line (argc = command line arg(s)?)
 
   // Open directory
 
+  DIR *dir;
+  struct dirent *item;
+  dir = opendir(".");
+
+  if (dir == NULL)
+  {
+    printf("oops!");
+    exit(1);
+  }
+  
   // Repeatly read and print entries
 
+  while(!((item=readdir(dir))=='\0'))
+  {
+    printf("%s\n", item->d_name);
+  }
+
   // Close directory
+
+  closedir(dir);
 
   return 0;
 }
