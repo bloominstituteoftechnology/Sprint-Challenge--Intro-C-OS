ebce4bb2ee50efca55882277ac55dacd94de0267
Sneha Thadani <snehacopper@gmail.com>
2018-03-09 08:38:13 -0800

    Mostly done

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -8,11 +9,16 @@
 void printEntries(char* dirname) {
 	DIR* handle = opendir(dirname);
 	struct dirent* entry = NULL;
+	struct stat buf;
 	do {
 		entry = readdir(handle);
 		if(entry == NULL)
 			break;
-		printf("%s\n", entry->d_name);
+		stat(entry->d_name, &buf);
+		if((buf.st_mode & S_IFDIR) == 0)
+			printf("%lld\t%s\n", buf.st_size, entry->d_name);
+		else
+			printf("<DIR>\t%s\n", entry->d_name);
 	} while(entry != NULL);
 	closedir(handle);
 }
