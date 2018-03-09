c78abbd6fe22b8771acf1b7f63ab6538262956a6
root <root@Capri-HP.localdomain>
2018-03-09 14:27:28 -0500

    times up

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
 
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
-
-  return 0;
-}
+  DIR *dir;
+  
+  printf("There are %d command line argument(s):\n", argc);
+  for ( int i = 0; i < argc; i++) {
+    printf("   %s\n", argv[i]);
+  }
+  if(argc < 2) 
+  {
+  dir = opendir("."); 
+  } else 
+  {
+  dir = opendir(argv[1]); 
+  }
+  
+  if ( dir == NULL)
+  {
+    printf("Not Found");
+    exit(0);
+  } else 
+  {  do 
+  {
+    printf( "%s\n", readdir(dir)->d_name);
+  }
+    while (readdir(dir)!=NULL);
+  }
+  
+   closedir(dir);
+   return 0;
+  }
