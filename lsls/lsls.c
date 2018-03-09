0c62af5297806804941cec355ca4a34563e0fbdd
Ting Wang <felix.wang.1026@gmail.com>
2018-03-09 11:58:54 -0500

    print the current directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,44 @@
 #include <stdio.h>
 #include <dirent.h>
 
+int printDir(char *path)
+{
+  DIR *d;
+  struct dirent *dir;
+  d = opendir(path);
+  if (d)
+  {
+    while ((dir = readdir(d)) != NULL)
+    {
+      printf("%s\n", dir->d_name);
+    }
+    closedir(d);
+  }
+  else
+  {
+    printf("No such \"%s\" directory found\n", path);
+    exit(1);
+  }
+  return 0;
+}
+
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  char *path;
 
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
+  if (argc == 1)
+  {
+    path = ".";
+    printDir(path);
+  }
+  else
+  {
+    path = argv[1];
+    printDir(path);
+  }
 
   return 0;
 }
