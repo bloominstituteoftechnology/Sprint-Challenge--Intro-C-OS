cb249bc291286b1e9f034ad8a4d4055b86741c28
JasonCorp84 <csababalogh84@gmail.com>
2018-03-09 10:27:28 -0800

    lists files from current directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,55 @@
+// #include <stdio.h>
+// #include <dirent.h>
+
+// /**
+//  * Main
+//  */
+// int main(int argc, char **argv)
+// {
+//   // Parse command line
+
+//   // Open directory
+
+//   // Repeatly read and print entries
+
+//   // Close directory
+
+//   return 0;
+// }
+
+
 #include <stdio.h>
 #include <dirent.h>
 
-/**
- * Main
- */
-int main(int argc, char **argv)
+
+int printFileList(char *path) {
+  DIR *d;
+    struct dirent *dir;
+    d = opendir(".");
+    if (d) {
+      while ((dir = readdir(d)) != NULL) {
+         if (dir->d_type == DT_REG) // DT_REG == A regular file
+    {
+      printf("%s\n", dir->d_name); 
+    }
+      }
+      closedir(d);
+    }
+    return(0);
+}
+
+int main(int argc, char *argv[]) 
 {
-  // Parse command line
+    char *path;
 
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
-
-  return 0;
+   if (argc == 1)
+    {
+    path = ".";
+    printFileList(path);
+    }
+    else
+    {
+      path=argv[1];
+      printFileList(path);
+    }
 }
