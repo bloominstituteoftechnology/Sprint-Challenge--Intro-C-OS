e8f459f04b15f807c83ad5a441c3c30c2d8426d3
JASPINDER SING <jaspindersingh83@gmail.com>
2018-03-09 11:49:39 -0700

    Completed-No Stretch

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -4,15 +4,38 @@
 /**
  * Main
  */
+void printContent(char *path) 
+{
+  struct dirent *ent;
+  DIR *d = opendir(path);
+
+  if(!d)
+  {
+    printf("No directory found at %s", path);
+  } 
+  else 
+  {
+    while ((ent = readdir(d)) != NULL)
+    {
+      printf("%s\n", ent->d_name); 
+    }
+    closedir(d);
+  }
+}
 int main(int argc, char **argv)
 {
-  // Parse command line
+  char *path;
 
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
+  if (argc != 1)
+  {
+    path = argv[1];
+    printContent(path);
+  }
+  else
+  {
+    path = ".";
+    printContent(path);
+  }
 
   return 0;
 }
