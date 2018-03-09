34f309b3511a4ce4f41e1643243f31d8f4e9ee58
atiffany <abby.tiffany@gmail.com>
2018-03-09 09:37:08 -0800

    code to print current dir ls

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include <dirent.h>
 
 /**
@@ -9,10 +11,23 @@ int main(int argc, char **argv)
   // Parse command line
 
   // Open directory
+  DIR *dir;
+  struct dirent *sd;
 
+  dir = opendir(".");
+  // handle error if null
+  if (dir == NULL)
+  {
+    printf("Error! Cannot open directory");
+    exit(1);
+  }
   // Repeatly read and print entries
 
+  while ( (sd = readdir(dir)) != NULL)
+  {
+    printf("%s\n", sd->d_name);
+  }
   // Close directory
-
+  closedir(dir);
   return 0;
 }
