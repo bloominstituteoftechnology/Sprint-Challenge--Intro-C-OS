14109d562dc5263b040e5698c1e13fe859dcd650
atiffany <abby.tiffany@gmail.com>
2018-03-09 10:58:33 -0800

    working on reading path, not operational yet

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -11,20 +11,34 @@ int main(int argc, char **argv)
   // Open directory
   DIR *dir;
   struct dirent *sd;
+  char location[80];
 
-  dir = opendir(".");
+  if (argv == NULL)
+  {
+    dir = opendir(".");
+  } else
+  {
+    for (int i = 0; argv[i] != '\0'; i++)
+    {
+      printf("Dir: %s\n", argv[i]);
+      sprintf(location, "%s%s", location, argv[i]);
+    }
+    dir = opendir(location);
+  }
   // handle error if null
   if (dir == NULL)
   {
-    printf("Error! Cannot open directory");
+    printf("Error! Cannot open directory\n");
     exit(1);
   }
+  
   // Repeatly read and print entries
-
-  while ( (sd = readdir(dir)) != NULL)
+  while ( (sd = readdir(dir)) != NULL )
   {
     printf("%s\n", sd->d_name);
   }
+
+
   // Close directory
   closedir(dir);
   return 0;
