c1b5f549bf2914c405c47672a0daec223b6e7d26
Synsyxx <travis.simpson92@hotmail.com>
2018-04-06 13:14:16 -0500

    code was function, broke stuff, pushing before breaking more stuff!

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -8,11 +12,19 @@ int main(int argc, char **argv)
 {
   DIR *dir;
   struct dirent *entry;
-  char *currentDir = ".";
+  char currentDir = [1024];
   // Parse command line
+  if (argc > 1)
+  {
+    sprintf(currentDir, "%s", argv[1]);
+  }
+  else
+  {
+    sprintf(currentDir, "%s", ".");
+  }
 
   // Open directory
-
+  
   // Repeatly read and print entries
   if ((dir = opendir(currentDir)) == NULL)
   {
@@ -22,7 +34,18 @@ int main(int argc, char **argv)
   {
     while ((entry = readdir(dir)) != NULL)
     {
-      printf(" %s\n", entry->d_name);
+      struct stat buf;
+      char type[1024];
+      stat(entry->d_name, &buf);
+      if (entry->d_type == DT_DIR)
+      {
+        sprintf(type, "%s", "<DIR>");
+      }
+      else
+      {
+        sprintf(type, "%ld", buf.st_size);
+      }
+      printf("  %6s  %s\n", type, entry->d_name);
     }
     closedir(dir);
   }
