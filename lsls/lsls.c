a926e377a527db2e16d02460967ef12cb0a56f16
Synsyxx <travis.simpson92@hotmail.com>
2018-04-06 11:47:38 -0500

    lsls printing root directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,11 +6,26 @@
  */
 int main(int argc, char **argv)
 {
+  DIR *dir;
+  struct dirent *entry;
+  char *currentDir = ".";
   // Parse command line
 
   // Open directory
 
   // Repeatly read and print entries
+  if ((dir = opendir(currentDir)) == NULL)
+  {
+    perror("opendir error");
+  }
+  else
+  {
+    while ((entry = readdir(dir)) != NULL)
+    {
+      printf(" %s\n", entry->d_name);
+    }
+    closedir(dir);
+  }
 
   // Close directory
 
