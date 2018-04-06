dc2a8b576b0ec911f062135b9cb65c87803cb716
Samuel Cha <sunchipssam@gmail.com>
2018-04-06 09:31:58 -0700

    Added multiple CLI dirs support

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -10,31 +10,52 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-  char *path = argv[1];
-
-  // Open directory
-  DIR *directory = opendir(path);
-
-  /* check if directory is NULL */
-  if (directory == NULL)
+  for (int i = 0; i < argc; i++)
   {
-    printf("Directory << %s >> not found.\nError: %s\n", path, strerror(errno));
-    exit(1);
-  }
+    char *path = argv[i];
 
-  // Repeatly read and print entries
-  else
-  {
-    struct dirent *ent;
-
-    while ((ent = readdir(directory)) != NULL)
+    /* case when user provides no dirs */
+    if (i == 0 && argc == 1)
     {
-      printf("%s\n", ent->d_name);
+      path = ".";
+    }
+
+    if (i == 0 && argc != 1)
+    {
+      /* if user provides dirs */
+      /* skip zero-index argv  */
+    }
+
+    else
+    {
+      // Open directory
+      DIR *directory = opendir(path);
+
+      /* check if directory is NULL */
+      if (directory == NULL)
+      {
+        printf("<< PATH: %s >> not found.\nError: %s\n", path, strerror(errno));
+        exit(1);
+      }
+
+      // Repeatly read and print entries
+      else
+      {
+        struct dirent *ent;
+
+        printf("<< PATH: %s >>\n", path);
+
+        while ((ent = readdir(directory)) != NULL)
+        {
+          printf("%s\n", ent->d_name);
+        }
+      }
+
+      // Close directory
+      closedir(directory);
+      printf("\n\n");
     }
   }
-
-  // Close directory
-  closedir(directory);
 
   return 0;
 }
