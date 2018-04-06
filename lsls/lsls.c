17095c35b49731dc8f77cc64e547900e5f94e7e5
Guelmis Cortina <cguelmis@yahoo.com>
2018-04-06 12:40:39 -0400

    implement lsls functionality

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+	char 					*current_dir;
+	DIR 					*dirp;
+	struct dirent *dir_entry;
+
   // Parse command line
+	if (argc < 2)
+		current_dir = ".";
+	else
+		current_dir = argv[1];
 
   // Open directory
-
+	if (!(dirp = opendir(current_dir)))
+	{
+		printf("%s\n", "There was an error");
+		exit(0);
+	}
   // Repeatly read and print entries
-
+	while ((dir_entry = readdir(dirp)))
+	{
+		printf("%s\n", dir_entry->d_name);
+	}
   // Close directory
+  closedir(dirp);
 
   return 0;
 }
