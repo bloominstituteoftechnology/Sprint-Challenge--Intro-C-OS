d7261c2d23330acc819ccb38c00a94af292cab09
groov1234 <c.groover@outlook.com>
2018-04-06 11:35:09 -0400

    root dir printing

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,24 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  DIR *dir;
+  struct dirent *entry;
+  char *currentDir = ".";
   // Open directory
-
+  if((dir = opendir(currentDir)) == NULL)
+  {
+    perror("Can't Open Directory");
+  }
   // Repeatly read and print entries
-
-  // Close directory
+  else
+  {
+    while ((entry = readdir(dir)) != NULL)
+    {
+      printf(" %s\n", entry->d_name);
+    }
+    // Close directory
+    closedir(dir);
+  }
 
   return 0;
 }
