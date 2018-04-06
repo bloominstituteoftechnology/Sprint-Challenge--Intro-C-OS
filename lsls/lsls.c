1ba36ca62fda81ef58e84c2d39835afd8ca23dbe
Johnathan Justinn <jjustinn85@gmail.com>
2018-04-06 13:01:39 -0400

    printDir function and error message written

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <dirent.h>
 
+int printDir(char *path)
+{
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(path);
+    if (d)
+    {
+      while ((dir = readdir(d)) != NULL)
+      {
+        printf("%s\n", dir->d_name);
+      }
+      closedir(d);
+    }
+    else
+    {
+      printf("This directory \"%s\" not found\n", path);
+      exit(1);
+    }
+  return 0;
+}
+
 /**
  * Main
  */
