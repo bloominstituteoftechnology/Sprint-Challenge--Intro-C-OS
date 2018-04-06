146742050a157ba496c7241d2cc34bce40f7917a
brcampbell001 <brcampbell001@outlook.com>
2018-04-06 12:08:22 -0500

    Challenge 2 Completed

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 
-/**
- * Main
- */
+int printDir(char *path) {
+  
+  struct dirent *ent;
+  DIR *d;
+  d = opendir(path);
+  
+  if (d) {
+    while ((ent = readdir(d)) != NULL) {
+      printf("%s\n", ent->d_name);
+      }
+      closedir(d);
+      } else {
+        printf("%sThe directory in which you search was not found.\n", path);
+        exit(1);
+        }
+        return 0;
+}
+
 int main(int argc, char **argv)
 {
-  // Parse command line
-
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
-
-  return 0;
-}
+  
+  char *path;
+  
+  if (argc == 1) {
+    path = ".";
+    printDir(path);
+    } else {
+      path = argv[1];
+      printDir(path);
+      }
+      return 0;
+} 
