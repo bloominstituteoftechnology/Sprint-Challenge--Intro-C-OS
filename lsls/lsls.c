08180b46438e33a623d0015087dbc547402808cb
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-04-06 12:05:07 -0400

    Opened dir and printing entries

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -9,10 +9,16 @@ int main(int argc, char **argv)
   // Parse command line
 
   // Open directory
-
+  struct dirent *dir;
+  DIR *d = opendir(".");
   // Repeatly read and print entries
-
+  if (d) {
+    while((dir = readdir(d)) != NULL) {
+      printf("%s\n", dir->d_name);
+    }
+  }
   // Close directory
+  closedir(d);
 
   return 0;
 }
