e675e08317c7e597dba3d6f577d25487e616efae
groov1234 <c.groover@outlook.com>
2018-04-06 12:01:33 -0400

    printing directories ./lsls ./lsls .. ./lsls ../examples

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,28 +1,53 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
   DIR *dir;
   struct dirent *entry;
-  char *currentDir = ".";
-  // Open directory
-  if((dir = opendir(currentDir)) == NULL)
+  char currentDir[1024];
+  // Parse command line
+  if (argc > 1)
   {
-    perror("Can't Open Directory");
+    sprintf(currentDir, "%s", argv[1]);
+    // printf("%s", currentDir);
   }
-  // Repeatly read and print entries
+  else
+  {
+    sprintf(currentDir, "%s", ".");
+  }
+
+  // Open directory
+  // If cant open dir send error.
+  if ((dir = opendir(currentDir)) == NULL)
+  {
+    perror("ERROR");
+  }
+  // If can open dir then read inside.
   else
   {
     while ((entry = readdir(dir)) != NULL)
+    // For every result of readdir
+    // within the currentDirectory
+    // print a new line with it.
     {
-      printf(" %s\n", entry->d_name);
+      struct stat buf;
+      char type[1024];
+      stat(entry->d_name, &buf);
+      if (entry->d_type == DT_DIR)
+      {
+        sprintf(type, "%s", "<DIR>");
+      }
+      else
+      {
+        sprintf(type, "%ld", buf.st_size);
+      }
+      printf("  %6s  %s\n", type, entry->d_name);
     }
-    // Close directory
     closedir(dir);
   }
 
