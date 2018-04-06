0aff3b9ddd48b77b2fd5ee9f7b4dd17854114f48
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-04-06 13:00:39 -0400

    stretch goals completed?

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  char *p;
+  struct dirent *dir;
+  struct stat buf;
   // Parse command line
+  if (argc > 1) 
+  {
+    p = argv[1];
+  }
+  else
+  {
+    p = ".";
+  }
 
   // Open directory
-  struct dirent *dir;
-  DIR *d = opendir(".");
+  DIR *d = opendir(p);
   // Repeatly read and print entries
   if (d) {
     while((dir = readdir(d)) != NULL) {
-      printf("%s\n", dir->d_name);
+      stat(dir->d_name, &buf);
+      (buf.st_mode & S_IFDIR) ? printf("%10s   %s\n", "<DIR>", dir->d_name) : printf("%10lu  %s\n", buf.st_size, dir->d_name);
     }
+  } else {
+    printf("The directory %s does not exist.\n", p);
   }
   // Close directory
   closedir(d);
