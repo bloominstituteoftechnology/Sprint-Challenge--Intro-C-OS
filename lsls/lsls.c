5d5a59e9bc35c7795a26fe637f1c19612771ffed
Clara Andrew <candrewwani@gmail.com>
2018-04-06 12:05:08 -0400

    challenge 2 completed

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  DIR *dir;
+  struct dirent *entry;
 
-  // Open directory
+  if (argc == 1) {
+    dir = opendir(".");
+  } else {
+    while(argv[1][0] == '/') {
+      argv[1]++;
+    }
+    dir = opendir(argv[1]);
+  }
 
-  // Repeatly read and print entries
+  if (dir == NULL) {
+    printf("There seems to be an error opening the directory\n");
+    exit(0);
+  }
 
-  // Close directory
+  while((entry = readdir(dir)) != NULL) {
+    printf("%s\n", entry->d_name);
+  }
+
+  closedir(dir);
 
   return 0;
 }
