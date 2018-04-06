fae86522fe990c399035a27faf3dafd95c786c85
Elly <ellyhahn@gmail.com>
2018-04-06 09:50:15 -0700

    Change README.md and lsls/lsls.c

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -8,11 +8,22 @@ int main(int argc, char **argv)
 {
   // Parse command line
 
+  char defaultDir[2] = ".";
+  struct dirent *dirEntry;
+  DIR *dirName;
+
   // Open directory
+  dirName = argc < 2 ? opendir(defaultDir) : opendir(argv[1]);
+  if (dirName == NULL) {
+    printf("Failed to open directory");
+    return 1;
+  }
 
   // Repeatly read and print entries
-
+  while ((dirEntry = readdir(dirName)) != NULL){
+    printf("Entry - %s\n", dirEntry->d_name);
+  }
   // Close directory
-
+  closedir(dirName);
   return 0;
 }
