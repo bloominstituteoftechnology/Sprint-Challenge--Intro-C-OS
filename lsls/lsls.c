45d900afc0adbee115f80be9a8432a29b807e50b
Samuel Cha <sunchipssam@gmail.com>
2018-04-06 09:00:27 -0700

    Implemented lsls with one CLI arg

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 /**
  * Main
@@ -7,12 +10,31 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *path = argv[1];
 
   // Open directory
+  DIR *directory = opendir(path);
+
+  /* check if directory is NULL */
+  if (directory == NULL)
+  {
+    printf("Directory << %s >> not found.\nError: %s\n", path, strerror(errno));
+    exit(1);
+  }
 
   // Repeatly read and print entries
+  else
+  {
+    struct dirent *ent;
+
+    while ((ent = readdir(directory)) != NULL)
+    {
+      printf("%s\n", ent->d_name);
+    }
+  }
 
   // Close directory
+  closedir(directory);
 
   return 0;
 }
