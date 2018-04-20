aa7faee005ec4675d73fc707500f4ffdf585fa0a
Giraud Julemis <giraud.julemis@gmail.com>
2018-04-20 12:57:47 -0400

    Success printing out the contents of the current directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,24 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
   // Open directory
-
   // Repeatly read and print entries
-
   // Close directory
+  DIR *dir;
+  struct dirent *dp;
+
+  if ((dir = opendir(".")) == NULL) {
+    perror("Cannot open .");
+    return 0;
+  }
+
+  do {
+    if ((dp = readdir(dir)) != NULL) {
+      (void) printf("%s\n", dp->d_name);
+    }
+  } while((dp = readdir(dir)) != NULL);
+
+  closedir(dir);
 
   return 0;
 }
