710de3f09e74c43d4476002bd0d7e346bdddd779
Charlie Sparks <csparks@antioch.edu>
2018-04-20 11:46:39 -0700

    finished logic for getting current directory read

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
+
+#include <sys/types.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  //line for commit
   // Parse command line
+  DIR *dir;
+  struct dirent *sd;
+  
+  dir = opendir(".");
+  if (dir == NULL){
+    printf("Error! Unable to open directory. \n");
+    exit(1);
+  }
+  
+  if ((sd = readdir(dir) == NULL))
+  {
+    printf("sd is null");
+  } else {
+    printf("sd isn't null");
+  }
 
-  // Open directory
-
-  // Repeatly read and print entries
+  // while((sd = readdir(dir)) != NULL)
+  // {
+  //   printf(">> %s\n", sd->d_name);
+  // }
 
   // Close directory
-
+  closedir(dir);
   return 0;
 }
