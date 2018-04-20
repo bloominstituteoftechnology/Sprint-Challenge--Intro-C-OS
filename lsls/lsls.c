f99dfe3443af448cd5ddc2072e44dd8989b7f4f7
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-04-20 14:02:55 -0400

    Forgot Error Handling for opendir()
    Never parsed the command line, not really I see why that is necessary, so I'm not going to.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -14,7 +14,6 @@ int main(int argc, char **argv)
     //DIR data type represents a directory stream.
     //Use opendir to open a directory stream
     DIR *d;
-    //Check if no argument is provided to command line
     //Assign d pointer the result of opendir
     d = opendir(".");
     printf("Directory: Current\n");
@@ -50,22 +49,20 @@ int main(int argc, char **argv)
     {
       DIR *d;
       d = opendir(argv[i]);
+      if (d == NULL) {
+        printf("Error Opening Directory %s\nExiting Program\n", argv[i]);
+        exit(0);
+      }
       printf("Directory: %s\n", argv[i]);
 
       struct dirent *ent;
       while ((ent = readdir(d)) != NULL)
       {
-        // store filename in pointer
         char *filename = (*ent).d_name;
-        // struct stat type pointer
         struct stat *buffer;
-        // allocate size the stat struct for size pointer
         buffer = malloc(sizeof(struct stat));
-        // obtain info of the filename
         stat(filename, buffer);
-        // store size of buffer
         int bytes = (*buffer).st_size;
-        // check if file or directory
         if (((*buffer).st_mode & S_IFDIR) == 0) printf("%5d  %s\n", bytes, filename);
         else printf("<DIR>  %s\n", filename);
         free(buffer);
