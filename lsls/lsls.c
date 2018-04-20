c02fd1c562e6c33414d293db295b2ecd21a6cbde
shobanavv <shobanavv@gmail.com>
2018-04-20 09:34:38 -0700

    initial try

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <dirent.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  int i;
   // Parse command line
-
+  DIR *d = opendir(".");
   // Open directory
-
+  printf("There are %d command line argument(s):\n", argc);
   // Repeatly read and print entries
-
+  for (i = 0; i < argc; i++) {
+        printf("   %s\n", argv[i]);
+    }
   // Close directory
-
+  closedir(d);
   return 0;
 }
+
+
+    
+
+    
