46e6d7c3d571c71f6c256c9fa7651d16c19c8acf
ashleijo <shuryjo@gmail.com>
2018-04-20 12:11:35 -0400

    adding functionality to check if directory exists
    prints error message if it doesnt, prints nothing if it does

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,18 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  DIR *checkDirectory;
+  char defaultDir[2] = ".";
   // Open directory
-
+  checkDirectory = argc < 2 ? opendir(defaultDir) : opendir(argv[1]);
+  if (checkDirectory == NULL)
+  {
+    printf("'%s' is not a valid directory!", argv[1]);
+    return 1;
+  }
   // Repeatly read and print entries
 
   // Close directory
-
+  closedir(checkDirectory);
   return 0;
 }
