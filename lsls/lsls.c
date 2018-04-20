82f64c398e5fd87ae127e10d5cfd89d2d4068f10
Jonathan Bry <jonathanbry@comcast.net>
2018-04-20 10:39:24 -0700

    done i think? parsed, opened, explored files, closed dir

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,25 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
-
-  return 0;
+ if (argc == 1) {
+   struct dirent *directoryent;
+   DIR *df = opendir(".");   // Open directory
+   while((directoryent = readdir(df)) != NULL) {
+     printf("%s\n%d", directoryent->d_name, directoryent->d_type);
+   }
+   closedir(df);  // Close directory
+ }
+ return 0;
 }
+      // int dirp = opendir(".");
+      // while (dirp) {
+      //   int errno = 0;
+      //   if ((readdir(dirp)) != NULL) {
+
+      //   }
+      // }
+      // DIR *opendir(const char *dirent);
+
+          // readdir(argc);
+          //   for( i = 0; i < argc; i++ ) 
+          //   printf_s( " argc[%d]   %s\n", argc ); // Repeatly read and print entries
