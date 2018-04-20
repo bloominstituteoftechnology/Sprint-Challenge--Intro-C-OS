36d1ea8c138adb5aec83298ff3a74341572903f2
Ronnie Miksch <fakepreferences@gmail.com>
2018-04-20 09:23:37 -0700

    Broke repeated print dirs code into a function

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -2,13 +2,23 @@
 #include <dirent.h>
 #include <errno.h>
 
+void printDirs(DIR *d)
+{
+  struct dirent *entry;
+  entry = readdir(d);
+  while(entry != NULL) {
+    printf("%s\n", entry->d_name);
+    entry = readdir(d);
+  }
+  closedir(d);
+}
+
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
   // Parse command line
-  struct dirent *entry;
   if(argc > 2) {
     // printf("%d\n%s\n%s", argc, argv[0], argv[1]);
     perror("Too many arguments supplied");
@@ -19,32 +29,16 @@ int main(int argc, char **argv)
     if(d == NULL) {
       perror("Directory doesnt exist");
     } else {
-      entry = readdir(d);
-      while(entry != NULL) {
-        printf("%s\n", entry->d_name);
-        entry = readdir(d);
-      }
-      closedir(d);
+      printDirs(d);
     }
   } else {
     DIR *d = opendir(".");
     if(d == NULL) {
       perror("Directory doesnt exist");
     } else {
-      entry = readdir(d);
-      while(entry != NULL) {
-        printf("%s\n", entry->d_name);
-        entry = readdir(d);
-      }
-      closedir(d);
+      printDirs(d);
     }
   }
-
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
 
   return 0;
 }
