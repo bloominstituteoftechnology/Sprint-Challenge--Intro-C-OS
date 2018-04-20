904be2a224ae6ad8e759e726a3de7096e1ad71e1
Dixie Korley <dixiekorley@gmail.com>
2018-04-20 12:20:35 -0400

    Refactoring lsls.c

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -16,12 +18,37 @@ int main(int argc, char **argv)
 
   DIR *dir;
   struct dirent *dent;
-  char buffer[50];
-  strcpy(buffer, argv[1]);
-  dir = opendir(buffer);
-  if (dir != NULL) {
-    while((dent=readdir(dir)) != NULL)
-      printf(dent -> d_name);
+  struct stat buffer;
+  // char buffer[50];
+  // strcpy(buffer, argv[1]);
+
+  // if (dir != NULL) {
+  //   while((dent=readdir(dir)) != NULL)
+  //     printf(dent -> d_name);
+  // }
+  if (argc == 1)
+  {
+    dir = opendir(".");
+  }
+  else
+  {
+    while (argv[1][0] == '/')
+    {
+      argv[1]++;
+    }
+    dir = opendir(argv[1]);
+  }
+
+  if (dir == NULL)
+  {
+    printf("There is an error opening the directory \n");
+    exit(0);
+  }
+
+  while ((dent = readdir(dir)) != NULL)
+  {
+    stat(entry-- > d_name, &buffer);
+    if ()
   }
   close(dir);
   return 0;
