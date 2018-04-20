5adcf0ace3bc4afb7ae31e7e47a3476ed4fe8b2a
Mike Streltsoff <MStreltsoff@gmail.com>
2018-04-20 14:12:24 -0400

    final sprint commit

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
-
-
+#include <sys/stat.h>
 /**
  * Main
  */
@@ -13,18 +12,38 @@ int main(int argc, char **argv)
   DIR *dir;
 
   struct dirent *entry;
-  char *currentDir = ".";
+  char currentDir[1024];
 
   // Repeatly read and print entries
+  if (argc > 1) 
+  {
+    sprintf(currentDir, "%s", argv[1]);
+  } else {
+    
+      sprintf(currentDir, "%s", ".");
+    }
+
   if ((dir = opendir(currentDir)) == NULL)
+  
   {
     perror("There was an error opening this directory");
   } else {
   // Close directory
   while ((entry = readdir(dir)) != NULL)
   {
-    printf(" %s\n", entry-> d_name);
-  }
+    struct stat buf;
+    char type[1024];
+    stat(entry -> d_name, &buf);
+    if (entry -> d_type == DT_DIR)
+    {
+      sprintf(type, "%s", "<DIR>");
+    }
+    else
+    {
+      sprintf(type, "%ld", buf.st_size);
+    }
+     printf(" %8s\n", entry-> d_name);
+   }
   closedir(dir);
   }
 
