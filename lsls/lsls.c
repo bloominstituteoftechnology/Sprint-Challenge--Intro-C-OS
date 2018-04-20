45c97edbe3af35fc3e11903b41397fcc36d8fbef
Cliff Kang <inlovewithhim@gmail.com>
2018-04-20 10:36:48 -0700

    it seems to work

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  DIR *dir;
+  struct dirent *dp;
+  printf("argc number is %i", argc);
+  if (argc < 2) {
+    printf("Usage: lsls <dirname>\n");
+    return 1;
+  }
+
+  dir = opendir(argv[1]);
+  if (dir == NULL) {
+    printf("Cannot open directory %s\n", argv[1]);
+    exit(1);
+  }
+
+  while((dp  = readdir(dir)) != NULL) {
+    printf("[%s]\n", dp->d_name);
+  }
+  closedir(dir);
+  return 0;
+
   // Parse command line
 
   // Open directory
@@ -13,6 +34,33 @@ int main(int argc, char **argv)
   // Repeatly read and print entries
 
   // Close directory
-
-  return 0;
 }
+
+
+
+  // if ((dir = opendir (".")) == NULL) {
+  //   perror ("Cannot open.");
+  //   exit (1);
+  // }
+  // for (int i = 1; i < argc; i++) {
+  //   do {
+  //     errno = 0;
+  //     if ((dp = readdir(dir)) != NULL) {
+  //       if (stsrcmp(dp->d_name, argv) != 0) { 
+  //         continue; 
+  //       }
+
+  //     printf("[%s]\n", argv);
+  //     closedir(dir);
+  //     return;
+  //   }
+  //   } while (dp != NULL);
+
+  //   if (errno != 0) {
+  //     perror("error reading directory");
+  //   } else {
+  //     printf("failed to find %s\n", argv);
+  //     closedir(dir);
+  //   }
+  //   return;
+  // }
