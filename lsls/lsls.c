439a26cfa950e174374b7b5d3ac7bff82ce5f5f4
rawalhome@gmail.com <rawalhome@gmail.com>
2018-04-20 12:20:31 -0700

    Puni Rawal: Stretch-filesize..done

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,24 +1,35 @@
-
 #include <dirent.h> 
 #include <stdio.h> 
+#include <sys/stat.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
-  // printf("No. of parameters passed %d\n", argc);
-  // printf("parameter vector %s\n", argv);
-  
   //declare variables
   DIR *d;
   struct dirent *ent;
+  struct stat buf;
+  char *path;
+  path = argv[1];
 
   //open dir
-  d = opendir(argv[1]);
+  d = opendir(path);
 
   //loop through directory contents
   if (d) {
     while ((ent = readdir(d)) != NULL) {
-      printf("%s\n", ent->d_name);
+      char* fullpath = malloc(strlen(path) + strlen(ent->d_name) + 10);
+      strcat(fullpath, path);
+      strcat(fullpath, "/");
+      strcat(fullpath, ent->d_name);
+      // printf("full path name is %s\n", fullpath);
+      stat(fullpath, &buf);
+      // printf("file size is %lld\n", buf.st_size);
+      printf("%lld%s%s\n", buf.st_size, " ", ent->d_name);
+
+      free(fullpath);
+      
     }
     closedir(d);
   }
-  return(0);
+  return 0;
 }
