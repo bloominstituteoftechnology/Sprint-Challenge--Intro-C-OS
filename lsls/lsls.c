788b5227a8965e3db189a78cb9c597d47e936e67
Igor Yermak <igor.yermak@gmail.com>
2018-04-20 13:06:45 -0400

    Commit 1
    opens dir

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 
 /**
@@ -6,13 +7,21 @@
  */
 int main(int argc, char **argv)
 {
+  struct dirent *ent;
   // Parse command line
+  int d;
+  char *args;
 
+  for (d = 0; d < argc; d++)
+  {
+    // input transformed to args which opens specified dir.
+    args = argv[d];
+  }
   // Open directory
-
+  DIR *directory = opendir("args");
   // Repeatly read and print entries
 
   // Close directory
-
+  closedir(directory);
   return 0;
 }
