dc6ec52a74e96c810a6c4f34a1363877dcd66d22
ashleijo <shuryjo@gmail.com>
2018-04-20 13:35:27 -0400

    adding functionality to list files all files in checked directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,17 +7,21 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  struct dirent *directoryName;
   DIR *checkDirectory;
   char defaultDir[2] = ".";
   // Open directory
   checkDirectory = argc < 2 ? opendir(defaultDir) : opendir(argv[1]);
   if (checkDirectory == NULL)
   {
-    printf("'%s' is not a valid directory!", argv[1]);
+    printf("'%s' is not a valid directory.", argv[1]);
     return 1;
   }
   // Repeatly read and print entries
-
+  while ((directoryName = readdir(checkDirectory)) != NULL)
+  {
+    printf("%s\n", directoryName->d_name);
+  }
   // Close directory
   closedir(checkDirectory);
   return 0;
