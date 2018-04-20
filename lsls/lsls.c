74e28e224d9204c44e37b398ddcc39c1b80d4403
Ronnie Miksch <fakepreferences@gmail.com>
2018-04-20 09:19:59 -0700

    Finished basic requirements

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <errno.h>
 
 /**
  * Main
@@ -7,6 +8,37 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  struct dirent *entry;
+  if(argc > 2) {
+    // printf("%d\n%s\n%s", argc, argv[0], argv[1]);
+    perror("Too many arguments supplied");
+    return 1;
+  }
+  if(argc > 1) {
+    DIR *d = opendir(argv[1]);
+    if(d == NULL) {
+      perror("Directory doesnt exist");
+    } else {
+      entry = readdir(d);
+      while(entry != NULL) {
+        printf("%s\n", entry->d_name);
+        entry = readdir(d);
+      }
+      closedir(d);
+    }
+  } else {
+    DIR *d = opendir(".");
+    if(d == NULL) {
+      perror("Directory doesnt exist");
+    } else {
+      entry = readdir(d);
+      while(entry != NULL) {
+        printf("%s\n", entry->d_name);
+        entry = readdir(d);
+      }
+      closedir(d);
+    }
+  }
 
   // Open directory
 
