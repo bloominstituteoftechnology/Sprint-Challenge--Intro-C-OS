d172b7440f4be9603f0eceb646c978b0416d8d57
Dixie Korley <dixiekorley@gmail.com>
2018-04-20 12:43:10 -0400

    lsls displaying but haven't used parameters

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -15,10 +16,31 @@ int main(int argc, char **argv)
   // Repeatly read and print entries
 
   // Close directory
-
+  struct stat buffer;
+  int exists;
   DIR *dir;
   struct dirent *dent;
-  struct stat buffer;
+
+  dir = opendir(".");
+
+  if (dir == NULL)
+  {
+    fprintf(stderr, "Could'nt open \".\"\n");
+    exit(1);
+  }
+
+  for (dent = readdir(dir); dent != NULL; dent = readdir(dir))
+  {
+    exists = stat(dent->d_name, &buffer);
+    if (exists < 0)
+    {
+      fprintf(stderr, "%s not found \n", dent->d_name);
+    }
+    else
+    {
+      printf("%s %lld\n", dent->d_name, buffer.st_size);
+    }
+  }
   // char buffer[50];
   // strcpy(buffer, argv[1]);
 
@@ -26,30 +48,31 @@ int main(int argc, char **argv)
   //   while((dent=readdir(dir)) != NULL)
   //     printf(dent -> d_name);
   // }
-  if (argc == 1)
-  {
-    dir = opendir(".");
-  }
-  else
-  {
-    while (argv[1][0] == '/')
-    {
-      argv[1]++;
-    }
-    dir = opendir(argv[1]);
-  }
 
-  if (dir == NULL)
-  {
-    printf("There is an error opening the directory \n");
-    exit(0);
-  }
+  // if (argc == 1)
+  // {
+  //   dir = opendir(".");
+  // }
+  // else
+  // {
+  //   while (argv[1][0] == '/')
+  //   {
+  //     argv[1]++;
+  //   }
+  //   dir = opendir(argv[1]);
+  // }
 
-  while ((dent = readdir(dir)) != NULL)
-  {
-    stat(entry-- > d_name, &buffer);
-    if ()
-  }
-  close(dir);
+  // if (dir == NULL)
+  // {
+  //   printf("There is an error opening the directory \n");
+  //   exit(0);
+  // }
+
+  // while ((dent = readdir(dir)) != NULL)
+  // {
+  //   stat(entry-- > d_name, &buffer);
+  //   if ()
+  // }
+  closedir(dir);
   return 0;
 }
