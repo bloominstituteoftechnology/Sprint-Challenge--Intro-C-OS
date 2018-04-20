35ca0036434374211b8fde5b854852ef224e18dc
Peter Gray <peter@petergraycreative.com>
2018-04-20 10:23:47 -0500

    Began researching/writing directory opening reading code

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,11 +1,24 @@
-#include <stdio.h>
 #include <dirent.h>
+#include <errno.h>
+#include <stdio.h>
 
 /**
  * Main
  */
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+  // DIR *dir;
+  // struct dirent *entry;
+
+  // if ((dir = opendir(argv[argc])) == NULL)
+  //   perror("opendir() error");
+  // else {
+  //   puts("contents of root:");
+  //   while ((entry = readdir(dir)) != NULL) {
+  //     printf("  %s\n", entry->d_name);
+  //   }
+  //   closedir(dir);
+  // }
+
   // Parse command line
 
   // Open directory
