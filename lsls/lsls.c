f25420a60158216624bc776c4e5f7da208890fde
aceredesign <steven.magadan@hotmail.com>
2018-04-20 08:44:48 -0700

    Making DIR

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -7,8 +9,19 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *directory;
+
+  if(!(directory = argv[1])) {
+    directory = ".";
+  }
+
 
   // Open directory
+  DIR *ls_directory = opendir(directory);
+
+  if(ls_directory == NULL) {
+    printf("This directory does not exist: %s\n,", argv[1]);
+  };
 
   // Repeatly read and print entries
 
