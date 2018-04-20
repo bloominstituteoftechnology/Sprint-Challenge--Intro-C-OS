1168a689bc1f29f83774b2f32bd3383312a5274c
aceredesign <steven.magadan@hotmail.com>
2018-04-20 09:04:15 -0700

    Showing some directories

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <stdlib.h>
-#include <sys/stat.h>
+#include <sys/types.h>
 
 /**
  * Main
@@ -21,11 +21,19 @@ int main(int argc, char **argv)
 
   if(ls_directory == NULL) {
     printf("This directory does not exist: %s\n,", argv[1]);
+    exit(1);
   };
 
   // Repeatly read and print entries
-
+   struct dirent *entry;
+  entry = readdir(ls_directory);
+  
+  while (entry != NULL) {
+    printf("%s\n", entry->d_name);
+    entry = readdir(ls_directory);
+  };
   // Close directory
 
+  closedir(ls_directory);
   return 0;
 }
