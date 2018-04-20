0d0ed1cfada4874e06400b036b42c338b16117cb
Mike Streltsoff <MStreltsoff@gmail.com>
2018-04-20 13:18:09 -0400

    Program printing directory listing

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 #include <dirent.h>
 
+
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
   // Parse command line
-
   // Open directory
 
-  // Repeatly read and print entries
+  DIR *dir;
 
+  struct dirent *entry;
+  char *currentDir = ".";
+
+  // Repeatly read and print entries
+  if ((dir = opendir(currentDir)) == NULL)
+  {
+    perror("There was an error opening this directory");
+  } else {
   // Close directory
+  while ((entry = readdir(dir)) != NULL)
+  {
+    printf(" %s\n", entry-> d_name);
+  }
+  closedir(dir);
+  }
 
   return 0;
 }
