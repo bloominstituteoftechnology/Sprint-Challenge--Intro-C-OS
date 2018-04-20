9ee55a2d36623f5a86018c2e4d9f102a2528e287
Dani Tacheny <dani.tacheny@gmail.com>
2018-04-20 11:07:40 -0500

    lsls.c working

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+	DIR *opened;
 
-  // Open directory
+	// if only one argument is passed in, open current directory
+	if (argc == 1) {
+		opened = opendir(".");
+	} 
+	// if more than one argument is passed in open the specified directory
+	else {
+		argv++;
+		opened = opendir(*argv);
+	}
 
-  // Repeatly read and print entries
+	// if opendir encountered an error, print message and exit program
+	if (opened == NULL) {
+		printf("There was an error opening the directory.");
+		exit(1);
+	}
+
+	// read the opened directory
+	struct dirent *directory = readdir(opened);
+
+	// iterate through the files in the directory and print each
+	do {
+		printf("%s\n", directory->d_name);
+		directory = readdir(opened);
+	}
+	while (directory != NULL);
 
   // Close directory
+	closedir(opened);
 
   return 0;
 }
