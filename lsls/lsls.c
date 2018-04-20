5b59840cf601bd8e3e6f537b337cad3edcd51f60
Russell Leon Bates IV <russell.l.bates@gmail.com>
2018-04-20 08:50:53 -0700

    <Leon> First commit - ls output is working for a hardcoded path.
    * * *
    TO DO
    * * *
    * Add command line parsing.
    * Stretch Goals
    
    * * *
    RESOURCES
    * * *
    * http://pubs.opengroup.org/onlinepubs/009604599/functions/readdir.html
    * http://pubs.opengroup.org/onlinepubs/009695399/basedefs/dirent.h.html
    * https://www.gnu.org/software/libc/manual/html_node/Opening-a-Directory.html

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
 
 /**
  * Main
@@ -7,12 +8,23 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  char target_dir[] = ".";
   // Open directory
+  DIR *dirp;
+  struct dirent *dp;
+
+  if ((dirp = opendir(target_dir)) == NULL) {
+        perror("couldn't open that dir!");
+        return 0;
+    }
 
   // Repeatly read and print entries
-
+  do {
+    if ((dp = readdir(dirp)) != NULL) {
+      (void) printf("%s\n", dp->d_name);
+    }
+  } while (dp != NULL);
   // Close directory
-
+  closedir(dirp);
   return 0;
 }
