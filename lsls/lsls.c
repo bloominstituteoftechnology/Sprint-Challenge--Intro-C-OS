d564d1c8d9fdd8cd0bd589d9cfdfc6adeb054f80
Tyson Williams <tyson.j.williams@gmail.com>
2018-04-20 11:35:14 -0600

    Update main
    Create helper function to get directory listing
    Call helper funcion in main

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,35 +1,48 @@
 #include <stdio.h>
 #include <dirent.h>
-#include <dirent.h>
 
 /**
- * Main
+ * Get Directory Listing
  */
-int main(int argc, char **argv)
-{
-  // Parse command line
+int listdir(const char *path) {
+  DIR *dp;
+  struct dirent *ep;
+
+  // Open directory
+  dp = opendir(path);
+  if (dp == NULL) {
+    perror("Couldn't open the directory"); // print any errors
+    return -1;
+  }
+  // Repeatly read entries
+  while((ep = readdir(dp)))
+    printf("   %s\n", ep->d_name);
+
+  // Close directory
+  closedir(dp);
+  return 0;
+}
+
+/**
+ * Main Function to Print Listing
+ */
+int main(int argc, char **argv) {
+  int counter = 1;
+
+  // Parse and print command line
   int i;
-  
   printf("   File path: \n");
   for (i = 0; i < argc; i++) {
     printf("   %s\n", argv[i]);
   }
 
-  // Open directory
-  DIR *dp;
-  struct dirent *ep;
-  dp = opendir("./");
+  if (argc == 1)
+    listdir(".");
 
-  // Repeatly read and print entries
-  if (dp != NULL) {
-    printf("\n   Directory Listing: \n");
-    while (ep = readdir(dp)) {
-      printf("   %s\n", ep->d_name);
-    }
-  // Close directory
-    (void) closedir(dp);
-  } else {
-    perror("Couldn't open the directory"); // print any errors
+  // Repeatly print entries
+  while (++counter <= argc) {
+    printf("\n   Print Listing for %s\n", argv[counter-1]);
+    listdir(argv[counter-1]);
   }
 
   return 0;
