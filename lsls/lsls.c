0c5aaa6368d29234744e7fd089414de62f7689da
Res Yeg <shaqk702@gmail.com>
2018-04-20 08:17:27 -0700

    prints current directory contents

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  struct dirent *ent;
+
   // Parse command line
 
   // Open directory
-
+  DIR *d = opendir(".");
+  
   // Repeatly read and print entries
+  // readdir();
+  if (d == NULL) {
+    // return 0;
+    perror ("Cannot open .");
+    exit (1);
+  }
+  while ((ent = readdir(d)) != NULL) {
+    printf("%s\n",ent->d_name);
+  }
 
   // Close directory
+  closedir(d);
 
   return 0;
 }
