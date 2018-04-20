427dd4547076d6b785d12f56e368164e290db9a9
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-04-20 11:50:57 -0400

    lsls reading current directory correctly

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,18 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  
   // Open directory
-
+  DIR *dir;
+  struct dirent *dp;
+  char *file_name;
+  dir = opendir(".");
   // Repeatly read and print entries
-
+  while((dp=readdir(dir)) != NULL) {
+      file_name = dp->d_name;
+      printf("- %s\n", file_name);
+  }
   // Close directory
-
+  closedir(dir);
   return 0;
 }
