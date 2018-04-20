ba3a5dceb8bc1bae1681cfe2fa7168e4914840d8
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-04-20 12:54:57 -0400

    Testing: PASS acheived

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,44 @@
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  // no arguments provided, open current directory
+  if (argc == 1)
+  {
+    //DIR data type represents a directory stream.
+    //Use opendir to open a directory stream
+    DIR *d;
+    //Check if no argument is provided to command line
+    //Assign d pointer the result of opendir
+    d = opendir(".");
+    printf("Directory: Current\n");
 
-  // Open directory
+    //Pointer to
+    struct dirent *ent;
+    //NULL means no more entries
+    while ((ent = readdir(d)) != NULL)
+    {
+      printf("%s\n", (*ent).d_name);
+    }
+    printf("\n");
+    closedir(d);
+  }
+  else
+  {
+    for (int i = 1; i < argc; i++)
+    {
+      DIR *d;
+      d = opendir(argv[i]);
+      printf("Directory: %s\n", argv[i]);
 
-  // Repeatly read and print entries
-
-  // Close directory
+      struct dirent *ent;
+      while ((ent = readdir(d)) != NULL)
+      {
+        printf("%s\n", (*ent).d_name);
+      }
+      printf("\n");
+      closedir(d);
+    }
+  }
 
   return 0;
 }
