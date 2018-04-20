b5bd56134596ddbbad6ff039e13ece09b3be13e6
Res Yeg <shaqk702@gmail.com>
2018-04-20 10:24:41 -0700

    file size printing

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,36 +1,40 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  struct dirent *ent;
-
   // Parse command line
-  int i;
+  // int i;
   char *args;
   // printf("There are %d command line argument(s):\n", argc);
   // printf( "argv[0]:  %s\n", argv[0] );
-
-  for (i = 0; i < argc; i++) {
-    // printf("   %s\n", argv[1]);
-    args = argv[i];
+  // printf( "argv[1]:  %s\n", argv[1] );
+  if (argc == 1) {
+    args = argv[0];
+  } else if (argc == 2) {
+    args = argv[1];
   }
 
   // Open directory
   DIR *d = opendir(args);
-  
-  // Repeatly read and print entries
-  // readdir();
   if (d == NULL) {
-    perror ("Cannot open .");
+    perror ("Directory does not exist.");
     exit (1);
   }
+  
+  // Repeatly read and print entries
+  struct dirent *ent;
+  struct stat buf;
+
   while ((ent = readdir(d)) != NULL) {
-    printf("%s\n",ent->d_name);
+    // stat(d, &buf);
+    printf("file size is %10lld", buf.st_size);
+    printf(" %s\n", ent->d_name);
   }
 
   // Close directory
