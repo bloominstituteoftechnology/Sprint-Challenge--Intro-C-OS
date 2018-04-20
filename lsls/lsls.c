8e648407418f225aa72f53dda771847bf7b9933e
Mephestys <mephestys@gmail.com>
2018-04-20 10:44:26 -0600

    Main goals completed. - If directory is provided, prints contents of that directory. - Otherwise will print contents of current directory. - If nonexistent directory provided, prints helpful error and exits. - Also added gitignore to prevent binaries from being tracked.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * Main
@@ -7,12 +10,26 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  printf("Directory: ");
+
+  for(int i = 1; i < argc; i++) {
+    printf("%s\n", argv[i]);
+  }
 
   // Open directory
+  DIR *d = opendir(argv[1] ? argv[1] : ".");
+  struct dirent *dir;
 
-  // Repeatly read and print entries
-
-  // Close directory
-
+  if (errno != 0) {
+    printf("Error: %s\n", strerror(errno));
+    exit(errno);
+  } else if (d) {
+    // Repeatly read and print entries
+    while ((dir = readdir(d)) != NULL) {
+      printf("%s\n", dir->d_name);
+    }
+    // Close directory
+    closedir(d);
+  }
   return 0;
 }
