8bc1e7d9cac81fb9a913867b97e50f997f4de411
Aaron Burk <aarondburk@gmail.com>
2018-04-20 09:56:19 -0500

    add lsls basic functionality
    - still working on error handling

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <errno.h>
 
+static void lookup(const char *arg)
+{
+	DIR *dirp;
+	// struct dirent *dp;
+
+	if ((dirp = opendir(arg)) == NULL) {
+		perror("no such directory");
+	}
+}
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
-
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
-
+	if (argc == 1) {
+		DIR *d;
+		struct dirent *dir;
+		d = opendir(".");
+		while((dir = readdir(d)) != NULL)
+		{
+			printf("%s\n", dir->d_name);
+		}
+		closedir(d);
+	}
+	if (argc == 2){
+		lookup(argv[1]);
+		DIR *d;
+		struct dirent *dir;
+		d = opendir(argv[1]);
+		while ((dir = readdir(d)) != NULL)
+		{
+/*			if (!dir->d_name) {
+				printf("No such directory as  %s\n", argv[1]);
+				} 
+*/
+			printf("%s\n", dir->d_name);
+		}
+		closedir(d);
+	}
   return 0;
 }
