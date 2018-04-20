bd128177269fa34243818bf48597d4b94745e2ae
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-04-20 13:50:19 -0500

    Challenge 2
    - in lsls.c
    - remove commented test code
    - check against argc, return error if too many arguments supplied (>2)
    - open the directory supplied by ternary
    - loop over directory entries
    - print file strings
    - handle errors
    - close directory
    - test in CLI (pass)

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <dirent.h>
 
-/**
- * Main
- */
 int main(int argc, char **argv)
-{
-  // Parse command line
-
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
-
+{ 
+  /* error handling for multiple CLI arguments */
+  /* also implemented to prevent compilation warning from unused argc param */
+  if (argc > 2) {
+    printf("Too many arguments supplied by the user. Please try again.\n");
+    return 0;
+  }
+  /* open specified directory if supplied by user, otherwise opens current dir */
+  DIR *directory = opendir(argv[1] ? argv[1] : ".");
+  /* conditional that executes if directory exists...should always execute */
+  if (directory) {
+    /* retrieve sturct dirent (functionality provided by dirent header */
+    struct dirent *next = readdir(directory);
+    /* loop until NULL indicates no more directory entries */
+    while ((next = readdir(directory)) != NULL) {
+      /* print the string from the accessed member */
+      printf("%s\n", next->d_name);
+    }
+  /* internal error handling */
+  } else {
+    printf("An error occured while reading the data. Please try again.");
+  }
+  /* close the directory */
+  closedir(directory);
+  /* return out of program */
   return 0;
 }
