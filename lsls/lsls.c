86df809fb0f5c48bef601de42a5ce3abad4f9320
Roy <Roy.tanjx@gmail.com>
2018-04-20 15:43:39 -0400

    Write Program in C

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -8,9 +10,33 @@ int main(int argc, char **argv)
 {
   // Parse command line
 
+DIR *dir;
+struct dirent *ent 
+char currentDir[1024];
+
+if (argc > 1)
+{
+  sprintf(currentDir, "%s", argv[1]);
+} else {
+  sprintf(currentDir, "%s", ".");
+}
+
+Dir *dir = opendir(".");
+printf("Directory: Current\n");
+  
   // Open directory
 
+if ((dir = opendir(currentDir)) == NULL) {
+  perror("Error opening Directory");
+}
+
+while ((dp = readdir(dir)) != NULL) {
+  printf("%s\n", dp -> d_name)
+}
+
   // Repeatly read and print entries
+
+int closeDir(DIR *dir);
 
   // Close directory
 
