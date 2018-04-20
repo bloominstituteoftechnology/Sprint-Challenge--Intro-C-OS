416389a4ce67055824f371b86243e8bfd1b4b6f3
daniel <endtrippy@gmail.com>
2018-04-20 12:42:06 -0700

    Trim Code

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,7 +6,6 @@
  */
 int main(int argc, char **argv)
 {
-  
   struct dirent *de;
 
  	if(argv[1] == NULL) {
@@ -17,14 +16,13 @@ int main(int argc, char **argv)
     closedir(dr);
     return 0;
 		}
-
- 		else if(argv[1] != NULL) {
- 			char *x = argv[1];
- 			DIR *dr = opendir(x);
-	    while ((de = readdir(dr)) != NULL){
-				printf("%s\n", de->d_name);
-	    }
-	    closedir(dr);
-	    return 0;
- 		}
+	else {
+			char *x = argv[1];
+			DIR *dr = opendir(x);
+    while ((de = readdir(dr)) != NULL){
+			printf("%s\n", de->d_name);
+    }
+    closedir(dr);
+    return 0;
+	}
 }
