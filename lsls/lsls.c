04cf40a1114fd513533804d424cb042595f65f7b
Nate152 <35231160+Nate152@users.noreply.github.com>
2018-04-20 15:47:42 -0400

    Challenge 2 Complete
    -Finished code in lsls.c

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h> /* included for exit() use requested in specs */
 
-/**
- * Main
- */
 int main(int argc, char **argv)
-{
-  // Parse command line
-
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
-
+{ 
+  if (argc > 2) {
+    printf("Too many arguments supplied by the user. Please try again.\n");
+    exit(-1);
+  }
+  DIR *directory = opendir(argv[1] ? argv[1] : ".");
+  if (directory) {
+    struct dirent *next = readdir(directory);
+    while ((next = readdir(directory)) != NULL) {
+      printf("%s\n", next->d_name);
+    }
+  } else {
+    printf("An error occured while reading the data. Please try again.\n");
+    exit(-1);
+  }
+  closedir(directory);
   return 0;
 }
