019b61db6968424fe0c2c15388801740bd5dca8f
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-04-20 14:06:08 -0500

    Final Edits
    - in lsls.c
    - clean up comments
    - add newline at end of printing in line 29, for formatting
    - final CLI test (pass)

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <dirent.h>
-#include <stdlib.h> /* included for exit() usage requested in specs */
+#include <stdlib.h> /* included for exit() use requested in specs */
 
 int main(int argc, char **argv)
 { 
-  /* error handling for multiple CLI arguments */
-  /* also implemented to prevent compilation warning from unused argc param */
+  /* custom error handling for extra CLI arguments */
+  /* also implemented to prevent compilation warning from unused (required) argc parameter */
   if (argc > 2) {
+    /* error message */
     printf("Too many arguments supplied by the user. Please try again.\n");
+    /* exit as instructed in specs */
     exit(-1);
   }
   /* open specified directory if supplied by user, otherwise opens current dir */
   DIR *directory = opendir(argv[1] ? argv[1] : ".");
-  /* conditional that executes if directory exists...should always execute */
+  /* conditional that executes if directory exists */
   if (directory) {
-    /* retrieve sturct dirent (functionality provided by dirent header */
+    /* retrieve struct dirent (function provided by dirent header */
     struct dirent *next = readdir(directory);
     /* loop until NULL indicates no more directory entries */
     while ((next = readdir(directory)) != NULL) {
       /* print the string from the accessed member */
       printf("%s\n", next->d_name);
     }
-  /* internal error handling */
+  /* error handling */
   } else {
-    printf("An error occured while reading the data. Please try again.");
+    /* error message */
+    printf("An error occured while reading the data. Please try again.\n");
     /* exit as instructed in specs */
     exit(-1);
   }
