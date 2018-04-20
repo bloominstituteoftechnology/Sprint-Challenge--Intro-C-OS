fa07848fbb9b7b59a4ee804391ed4d302f1e4f21
Russell Leon Bates IV <russell.l.bates@gmail.com>
2018-04-20 09:20:32 -0700

    ./lsls is MVP-complete, still need to do essays

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,24 +7,31 @@
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
-  char target_dir[] = ".";
-  // Open directory
   DIR *dirp;
   struct dirent *dp;
+  char target_dir[1024] = ".";
+
+  if (argc > 2) {
+    printf("The number of args from the command line is: %i", argc);
+    perror("Don't provide multiple arguments!");
+    return 0;
+  };
+  
+  if (argc == 2) {
+    sprintf(target_dir, argv[1]);
+  };
 
   if ((dirp = opendir(target_dir)) == NULL) {
         perror("couldn't open that dir!");
         return 0;
     }
 
-  // Repeatly read and print entries
   do {
     if ((dp = readdir(dirp)) != NULL) {
       (void) printf("%s\n", dp->d_name);
     }
   } while (dp != NULL);
-  // Close directory
+
   closedir(dirp);
   return 0;
 }
