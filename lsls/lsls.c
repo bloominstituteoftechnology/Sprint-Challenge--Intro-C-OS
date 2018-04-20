e078e41c79b6593b91fba32ee12f2221df9d3e24
Richard Reis <richardreeze1@gmail.com>
2018-04-20 10:52:15 -0700

    code works, but I don't get it

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -9,10 +9,20 @@ int main(int argc, char **argv)
   // Parse command line
 
   // Open directory
-
+  struct dirent *de;
+  DIR *dr = opendir(".");
+  
   // Repeatly read and print entries
+  if (dr == NULL)
+  {
+    printf("Could not open current directory" );
+    return 0;
+  }
 
+  while ((de = readdir(dr)) != NULL)
+    printf("%s\n", de->d_name);
+  
   // Close directory
-
+  closedir(dr);
   return 0;
 }
