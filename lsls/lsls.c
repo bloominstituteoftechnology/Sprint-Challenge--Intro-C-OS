47d7a7e5810b95664941ba84a98b3943bf1ee095
Cliff Kang <inlovewithhim@gmail.com>
2018-04-20 10:46:35 -0700

    added case for when no args passed in

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -9,13 +9,16 @@ int main(int argc, char **argv)
 {
   DIR *dir;
   struct dirent *dp;
-  printf("argc number is %i", argc);
-  if (argc < 2) {
-    printf("Usage: lsls <dirname>\n");
+  if (argc == 0 || argc > 2) {
+    printf("Use by typing in: lsls <dirname>\n");
     return 1;
   }
+  if (argc == 1) {
+    dir = opendir(".");
+  } else {
+    dir = opendir(argv[1]);
+  }
 
-  dir = opendir(argv[1]);
   if (dir == NULL) {
     printf("Cannot open directory %s\n", argv[1]);
     exit(1);
@@ -26,41 +29,4 @@ int main(int argc, char **argv)
   }
   closedir(dir);
   return 0;
-
-  // Parse command line
-
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
 }
-
-
-
-  // if ((dir = opendir (".")) == NULL) {
-  //   perror ("Cannot open.");
-  //   exit (1);
-  // }
-  // for (int i = 1; i < argc; i++) {
-  //   do {
-  //     errno = 0;
-  //     if ((dp = readdir(dir)) != NULL) {
-  //       if (stsrcmp(dp->d_name, argv) != 0) { 
-  //         continue; 
-  //       }
-
-  //     printf("[%s]\n", argv);
-  //     closedir(dir);
-  //     return;
-  //   }
-  //   } while (dp != NULL);
-
-  //   if (errno != 0) {
-  //     perror("error reading directory");
-  //   } else {
-  //     printf("failed to find %s\n", argv);
-  //     closedir(dir);
-  //   }
-  //   return;
-  // }
