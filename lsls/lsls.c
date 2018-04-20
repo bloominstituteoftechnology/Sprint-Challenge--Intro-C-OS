5f0d3dd0a12f2c1a3f1a8b181894561fe503a555
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-04-20 13:22:44 -0400

    Bytes Printed

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 
 /**
  * Main
@@ -15,31 +17,57 @@ int main(int argc, char **argv)
     //Check if no argument is provided to command line
     //Assign d pointer the result of opendir
     d = opendir(".");
-    printf("Directory: Current\n");
+    printf(
+        "Directory: Current\n"
+        "bytes  file/directory\n");
 
-    //Pointer to
+    //Pointer to entry
     struct dirent *ent;
+
     //NULL means no more entries
     while ((ent = readdir(d)) != NULL)
     {
-      printf("%s\n", (*ent).d_name);
+      // store filename in pointer
+      char *filename = (*ent).d_name;
+      // struct stat type pointer
+      struct stat *buffer;
+      // allocate size the stat struct for size pointer
+      buffer = malloc(sizeof(struct stat));
+      // obtain info of the filename
+      stat(filename, buffer);
+      // store size of buffer
+      int bytes = (*buffer).st_size;
+      printf("%5d  %s\n", bytes, filename);
+      
+      free(buffer);
     }
     printf("\n");
+
     closedir(d);
   }
+  //otherwise print all directories input on command line
   else
   {
     for (int i = 1; i < argc; i++)
     {
       DIR *d;
       d = opendir(argv[i]);
-      printf("Directory: %s\n", argv[i]);
+      printf(
+          "Directory: %s\n"
+          "bytes  file/directory\n",
+          argv[i]);
 
       struct dirent *ent;
       while ((ent = readdir(d)) != NULL)
-      {
-        printf("%s\n", (*ent).d_name);
-      }
+    {
+      char *filename = (*ent).d_name;
+      struct stat *buffer;
+      buffer = malloc(sizeof(struct stat));
+      stat(filename, buffer);
+      int bytes = (*buffer).st_size;
+      printf("%5d  %s\n", bytes, filename);
+      free(buffer);
+    }
       printf("\n");
       closedir(d);
     }
