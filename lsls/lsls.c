e4c17baef606bc36aad8b5264bfc0101182eb7d3
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-04-20 13:20:52 -0400

    File sizes printing correctly

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -18,16 +19,21 @@ int main(int argc, char **argv)
   DIR *dir;
   struct dirent *dp;
   char *file_name;
+  struct stat buff;
   dir = opendir(dir_arg);
   if (dir == NULL) {
     printf("There was an error reading that directory\n");
     return 0;
   }
-
+  
   // Repeatly read and print entries
+  stat(dir_arg, &buff);
   while((dp=readdir(dir)) != NULL) {
+      char fullpath[100];
       file_name = dp->d_name;
-      printf("| %s\n", file_name);
+      sprintf(fullpath, "%s/%s", dir_arg, file_name);
+      stat(fullpath, &buff);
+      printf("%10lldbytes | %s\n", buff.st_size, file_name);
   }
   // Close directory
   closedir(dir);
