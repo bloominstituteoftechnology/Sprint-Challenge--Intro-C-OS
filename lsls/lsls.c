7c377a6356fc7da60e41352ec3781a600b50dc84
Tyson Williams <tyson.j.williams@gmail.com>
2018-04-20 11:10:34 -0600

    Print contents for current directory listing

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <dirent.h>
 
 /**
  * Main
@@ -8,16 +9,28 @@ int main(int argc, char **argv)
 {
   // Parse command line
   int i;
-
+  
+  printf("   File path: \n");
   for (i = 0; i < argc; i++) {
     printf("   %s\n", argv[i]);
   }
 
   // Open directory
+  DIR *dp;
+  struct dirent *ep;
+  dp = opendir("./");
 
   // Repeatly read and print entries
-
+  if (dp != NULL) {
+    printf("\n   Directory Listing: \n");
+    while (ep = readdir(dp)) {
+      printf("   %s\n", ep->d_name);
+    }
   // Close directory
+    (void) closedir(dp);
+  } else {
+    perror("Couldn't open the directory"); // print any errors
+  }
 
   return 0;
 }
