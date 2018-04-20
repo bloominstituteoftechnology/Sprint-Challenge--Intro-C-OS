53f721392de455117e378268f4b28d4977a3512d
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-04-20 12:11:22 -0400

    Printing contents of given argument correctly

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,16 +7,27 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-  
+  char *dir_arg;
+  if (argc > 1) {
+    dir_arg = argv[1];
+  } else {
+    dir_arg = ".";
+  }
+
   // Open directory
   DIR *dir;
   struct dirent *dp;
   char *file_name;
-  dir = opendir(".");
+  dir = opendir(dir_arg);
+  if (dir == NULL) {
+    printf("There was an error reading that directory\n");
+    return 0;
+  }
+
   // Repeatly read and print entries
   while((dp=readdir(dir)) != NULL) {
       file_name = dp->d_name;
-      printf("- %s\n", file_name);
+      printf("| %s\n", file_name);
   }
   // Close directory
   closedir(dir);
