dc2465f7baf1598a89ec9f86ef5cd75fc8801546
shobanavv <shobanavv@gmail.com>
2018-04-20 12:46:41 -0700

    cleand up, WIP - parse command line args

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <sys/types.h>
 
 
 /**
@@ -8,32 +9,30 @@
  */
 int main(int argc, char **argv)
 {
-  // int i;
-  // printf("There are %d command line argument(s):\n", argc);
-  // // Parse command line
-  //   for (i = 0; i < argc; i++) {
+  int i;
+  printf("There are %d command line argument(s):\n", argc);
+  // Parse command line
+    for (i = 0; i < argc; i++) {
       
-  //       printf("   %s\n", argv[i]);
-  //       printf("argc %d", argc);
-  //   }
-  DIR *d;
-  // Open directory
-  int error = 0;
-  d = opendir(char *path);
-  // Repeatly read and print entries
-  if(d == NULL ) {
-    d = opendir(".");
-  }
-  struct dirent *ent;
-  if(d != NULL) {
-    while((ent = readdir(d)) != NULL) {
-      printf("%s\n", ent->d_name);
+        printf("   %s\n", argv[i]);
+        printf("argc %d", argc);
     }
-  } else if(error == 0) {
-    closedir(d);  
-    printf("error occured");
+  DIR *d;
+  struct dirent *ent;
+  
+  d = opendir(".");
+  if(d == NULL) 
+  {
+    printf("Error: Unable to open directory.\n");
     exit(1);
   }
+
+  while( (ent = readdir(d)) != NULL) 
+  {
+      printf("%s\n", ent->d_name);
+  }
+
+  
 
   // Close directory
   closedir(d);   
