ab8d24a58967bc0e551d0da837e5f5f4c27a49bd
JohnJohnX4 <johnjohnx4@gmail.com>
2018-05-08 15:47:33 -0400

    Sprint completed

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 #include <dirent.h>
 
-/**
- * Main
- */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  struct dirent *parentDirent;
+  DIR *parentDir;
 
-  // Open directory
+  if (argc < 2)
+  {
+    parentDir = opendir(".");
+    while ((parentDirent = readdir(parentDir)) != NULL)
+    {
+      printf("[%s]\n", parentDirent->d_name);
+    }
+    closedir(parentDir);
+  }
 
-  // Repeatly read and print entries
+  parentDir = opendir(argv[1]);
 
-  // Close directory
+  if (parentDir == NULL)
+  {
+    return 1;
+  }
+
+  while ((parentDirent = readdir(parentDir)) != NULL)
+  {
+    printf("[%s]\n", parentDirent->d_name);
+  }
+
+  closedir(parentDir);
 
   return 0;
 }
