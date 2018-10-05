3ea73576b5caa148f6b614ffa1c388591d603400
M.Real <mreal126@protonmail.com>
2018-10-05 13:25:08 -0400

    working without stat

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -10,26 +10,23 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
   DIR *dir;
   struct dirent *ent;
-  if (argc < 1) {
+  if (argc < 2) {
       if ((dir = opendir("./")) == NULL) {
           perror ("Cannot open ./");
-          exit (1);
       }
   } 
   // Open directory
   else {
-    if ((dir = opendir (argv[1])) == NULL) {
+    if ((dir = opendir(argv[1])) == NULL) {
           perror ("Cannot open ./");
-          exit (1);
       }
   }
 
   // Repeatly read and print entries
-
-  while ((ent = readdir(dir)) != NULL) {
+  ent = readdir(dir);
+  while (ent != NULL) {
     printf("%s\n", ent->d_name);
   }
 
