8ca9d96c54ae43d65b55e219040575a743cb2660
TangledTessellations <mcevoya@oregonstate.edu>
2018-10-05 10:58:30 -0700

    Add error checking for stat and format output

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
     strcpy(path, ".");
   }
 
-  printf("path: %s", path);
+
   // Open directory
   DIR *directory = opendir(path);
   // Repeatly read and print entries
@@ -30,8 +30,12 @@ int main(int argc, char **argv)
     struct dirent *entry = readdir(directory);
     while(entry){
       char *size_path = strcat(strcat(path, "/"), entry->d_name);
-      stat(size_path, &buffer);
-      S_ISDIR(buffer.st_mode) ? printf("<DIR> ") : printf("%li ", (intmax_t) buffer.st_size);
+      int rc = stat(size_path, &buffer);
+      if(rc == -1){
+        printf("Error getting file stats\n");
+        exit(1);
+      }
+      S_ISDIR(buffer.st_mode) ? printf("%10s ", "<DIR>") : printf("%10lld ", buffer.st_size);
       printf("%s\n", entry->d_name);
       entry = readdir(directory);
     }
