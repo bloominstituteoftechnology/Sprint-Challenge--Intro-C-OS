b63d3cfa61033936fa93c71c3d7ea4632fbfd30f
Kevin Tena <kevten09@gmail.com>
2018-10-05 12:32:38 -0500

    Challenge 2 completed

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
@@ -7,12 +8,26 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
   // Open directory
+  printf("Directory:\n");
+  DIR *directory = opendir(argv[1] ? argv[1] : ".");
+
+  if(!directory){
+      printf("Error occured. \n");
+      exit(-1);
+  }
 
   // Repeatly read and print entries
+  struct dirent *file = readdir(directory);
+
+
+  while ((file = readdir(directory)) != NULL)
+  {
+    printf("%s \n", file->d_name);
+  }
 
   // Close directory
+  closedir(directory);
 
   return 0;
 }
