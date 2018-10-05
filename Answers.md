c8771db7f5816cb77cf2eec80a75b65aad8a67ad
Marco A. Guzman <marcoguzman16@gmail.com>
2018-10-05 10:45:45 -0700

    tested examples. MVP /lsls

--- Answers.md
+++ Answers.md
@@ -1,4 +1,4 @@
 Challenge 1
 Name at least three things that an operating system is responsible for handling?
 
-The Operating System is constantly picking the process that will be given execution time on the CPU(scheduling processes), system calls, memory and CPU.
+The Operating System is constantly picking the process that will be given execution time on the CPU(scheduling processes), takes care of system calls, memory and CPU.
--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,49 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h> //added for exit process
 
 /**
  * Main
  */
+
 int main(int argc, char **argv)
 {
-  // Parse command line
+  // **Parse command line**
 
-  // Open directory
+  char *directname;
+  if (argc == 1){
+    // going to set dir that im looking for to "."
+    directname = ".";
+  }
+  else if (argc == 2){
+    directname = argv[1];
+  }
+  else{
+    fprintf(stderr, "ERROR: file unable to open\n");
+    exit(1);
+  }
+  // **Open directory**
 
-  // Repeatly read and print entries
+  //defining pointer
+  DIR *directory = opendir(directname);
+  // error checking
+  if (directory == NULL){
+    fprintf(stderr, "unable to open directory %s\n", directname);
+    exit(11); //different error (changing #)
+  }
 
-  // Close directory
+  // **Repeatly read and print entries**
+
+  //directory stream at "directory" is read, loop through directory contents
+  //readdir has current pointer and pointer for following element and "positions the directory stream at next entry(per docs"
+  struct dirent *entries; //pointer created
+  while ((entries = readdir(directory)) != NULL){
+    printf("%s\n", entries->d_name); //name of entry per docs (per dirent structure docs)
+  }
+
+  // **Close directory**
+
+  closedir(directory);
 
   return 0;
 }
