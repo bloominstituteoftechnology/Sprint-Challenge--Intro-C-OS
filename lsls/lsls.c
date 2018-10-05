15a56b9049dd035360e1f5850f96331e7ee1016e
CLN <cln@Sags-MacBook-Pro-2.local>
2018-10-05 13:41:51 -0400

    path changing works

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,48 @@
 #include <stdio.h>
 #include <dirent.h>
 
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
   // Parse command line
+  int i;
+  
+  
+
+    printf("There are %d command line argument(s):\n", argc);
+
+    for (i = 0; i < argc; i++) {
+        printf("   %s\n", argv[i]);
+        // printf( "  argv[%d]   %s\n", count, argv[count] );
+    }
 
   // Open directory
+DIR *dir;
+// DIR *opendir(char *path);
+struct dirent *ent;
 
+dir = opendir(argv[1]);
+ent = readdir(dir);
+
+
+
+if (dir == NULL) {
+        perror ("Cannot open .");
+        exit (1);
+}
   // Repeatly read and print entries
-
+while ((ent = readdir (dir)) != NULL) {
+  printf(" %s\n", ent->d_name);
+}
+    
+ 
   // Close directory
+closedir(dir);
 
   return 0;
 }
