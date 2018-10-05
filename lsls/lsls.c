aeeec4e885d17a671be7901f0599ef6ee9a5736d
cmok4290 <cmok4290@gmail.com>
2018-10-05 12:19:55 -0400

    added opendir, readdir, and closedir

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -9,10 +9,31 @@ int main(int argc, char **argv)
   // Parse command line
 
   // Open directory
+  DIR *dir;
+  dir = opendir(".");
+
+  if(dir == NULL)
+  {
+    perror("opendir");
+    exit(1);
+  }
 
   // Repeatly read and print entries
+  struct dirent *entry;
+
+  while((entry = readdir(dir)) != NULL)
+  {
+    printf(" %s\n", entry->d_name);
+  }
 
   // Close directory
+  if(closedir(dir) == -1)
+  {
+    perror("closedir");
+    exit(1);
+  }
+
+  closedir(dir);
 
   return 0;
 }
