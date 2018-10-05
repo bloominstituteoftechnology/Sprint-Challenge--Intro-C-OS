bc31506424548b9df2ec8d27d23d9cdda82baf1d
JJ Ashcraft <jj@jjashcraft.com>
2018-10-05 08:59:00 -0400

    JJAshcraft-CS11-Sprint-Challenge--Intro-C-OS *Initial Commit -Added answer to OS question -Added base program to print . directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,6 +6,19 @@
  */
 int main(int argc, char **argv)
 {
+
+  DIR *directory;
+  struct dirent *dir;
+  directory = opendir(".");
+  if (directory)
+  {
+    while ((dir = readdir(directory)) != NULL)
+    {
+      printf("%s\n", dir->d_name);
+    }
+    closedir(directory);
+  }
+  return (0);
   // Parse command line
 
   // Open directory
