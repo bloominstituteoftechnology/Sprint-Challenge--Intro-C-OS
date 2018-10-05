75f24a9fbbc26bd9f32bc5f8ed392190735f0eda
Vu Cao <stormer2716@yahoo.com>
2018-10-05 10:06:36 -0700

    Completed mvp

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,30 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *p;
+  if (argc == 1)
+  {
+    p = ".";
+  }
+  if (argc == 2)
+  {
+    p = argv[1];
+  }
 
-  // Open directory
-
+  DIR *pDir;
+  pDir = opendir(p);
+  if (pDir == NULL)
+  {
+    printf("Cannot open directory.\n");
+    return 1;
+  }
   // Repeatly read and print entries
-
+  struct dirent *pDirent;
+  while ((pDirent = readdir(pDir)) != NULL)
+  {
+    printf("%s\n", pDirent->d_name);
+  }
   // Close directory
-
+  closedir(pDir);
   return 0;
 }
