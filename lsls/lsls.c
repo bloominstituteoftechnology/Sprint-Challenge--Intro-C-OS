a1ad2fabd922c3e950a6eee2db04c884a89ee242
Katie Gorbell <katiegorbell@gmail.com>
2018-10-05 12:54:30 -0500

    built out iterations for subdirectories

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,19 +6,26 @@
  */
 int main(int argc, char **argv)
 {
+  DIR *dir;
+  struct dirent *path;
+
   printf("argc: %d\n", argc);
   // Parse command line
   for (int i = 0; i < argc; i++) {
     printf("path: %s\n", argv[i]);
     // Open directory
-    if (DIR *opendir(char *argv[i]) == NULL) {
+    if ((dir = opendir(*argv[i])) == NULL) {
       fprintf(stderr, "Open directory failed\n");
       exit(1);
+    } else {
+      // Repeatly read and print entries
+      while ((path = readdir(dir)) != NULL) {
+        printf("%s\n", path->d_name);
+      }
     }
   }
-  // Repeatly read and print entries
-
   // Close directory
+  closedir(dir);
 
   return 0;
 }
