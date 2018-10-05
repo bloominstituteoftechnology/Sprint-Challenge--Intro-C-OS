bebcab2e6698a937f4ead5e70a95bc4ee95725cc
Erin Meredith <ekmeredith@att.net>
2018-10-05 12:59:56 -0400

    fixed to accept args; mvp complete

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -8,22 +8,33 @@ int main(int argc, char **argv)
 {
   // Parse command line
   struct dirent *ent;
+
   int count;
+  printf("There are %d command line argument(s):\n", argc);
   for(count=0; count<argc; count++){
     puts(argv[count]);
   }
 
   // Open directory
-  DIR *dr = opendir(".");
-  if (dr == NULL) {
-    printf("Error: Could not open directory");
-    return 0;
+  if (count < 2){
+    DIR *dr = opendir(".");
+    while((ent = readdir(dr)) != NULL){
+      printf("%s\n", ent->d_name);
+    }
+  // Close directory
+    closedir(dr);
   }
+
+  else{
+    DIR *dr = opendir(argv[1]);
   // Repeatly read and print entries
   while((ent = readdir(dr)) != NULL){
     printf("%s\n", ent->d_name);
   }
   // Close directory
   closedir(dr);
+}
+  
+
   return 0;
 }
