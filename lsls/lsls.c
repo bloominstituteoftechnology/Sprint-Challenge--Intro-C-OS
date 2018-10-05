c184bc357175a8f8b7af77aea4abc6eb7a012f7c
sibhat <sibhat.temelso@gmail.com>
2018-10-05 10:21:11 -0700

    mvp done, and also Prints file size

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
 
 /**
  * Main
@@ -7,12 +10,55 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  struct dirent *ent;
+  struct stat buf;
 
-  // Open directory
+  if(argc >= 2){
+    for(int x = 0; x < argc; x++){
+      if (x > 0) {
 
-  // Repeatly read and print entries
+        printf("opening directory %s\n", argv[x]);
+        DIR *d = opendir(argv[x]);
+      
+        if(d == 0){
+          puts("error opening ");
+          exit(1);
+        }
+        ent = readdir(d);
+        while (ent){
+          char *path = argv[x];
+          char c[sizeof(argv[x]) + sizeof(ent->d_name)];
+          strcpy(c, path);
+          strcat(c, ent->d_name);
 
-  // Close directory
+          int stats = stat(c, &buf);          
+         
+          if (stats >= 0){
+            printf("%10lld %s\n", buf.st_size, ent->d_name);
 
+          }else{
+             printf("    %s\n", ent->d_name);
+          }
+          ent = readdir(d);
+        }
+        closedir(d);
+      }
+    }
+    
+  }else{
+    DIR *d = opendir("./");
+    printf("opening directory ./\n");
+    
+    if(d == 0){
+      puts("error opening ");
+      exit(1);
+    }
+    ent = readdir(d);
+    while (ent){
+      printf(" %s\n", ent->d_name);
+      ent = readdir(d);
+    }
+    closedir(d);
+  }
   return 0;
 }
