8d270076355691bf06b6ab12dcac840164ac07fd
emaquino44 <prexy44@yahoo.com>
2018-10-05 09:46:47 -0700

    added commandline code

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -8,6 +8,15 @@ int main(int argc, char **argv)
 {
   // Parse command line
 
+		int i;
+
+    printf("There are %d command line argument(s):\n", argc);
+
+    for (i = 0; i < argc; i++) {
+        printf("   %s\n", argv[i]);
+    }
+
+
   // Open directory
 
   // Repeatly read and print entries
