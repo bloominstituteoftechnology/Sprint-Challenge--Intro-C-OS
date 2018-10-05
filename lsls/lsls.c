d8847c3c03fe967eae1e179e57ba49e8643c51be
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-10-05 12:16:13 -0400

    lsls done, sprint done

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,39 @@
  */
 int main(int argc, char **argv)
 {
+  struct dirent *pDirent;
+  DIR *pDir;
+
   // Parse command line
+  if (argc == 1) 
+  {
+    pDir = opendir(".");
+  }
+  else if (argc == 2)   
+  {
+    pDir = opendir(argv[1]);
+  }
+  else    
+  {
+    printf("Usage: ./lsls.exe <directory name>\n");
+    return 1;
+  }
 
   // Open directory
+  if (pDir == NULL)  
+  {
+    printf("Can't open dir %s \n", argv[1]);
+    return 1;
+  }
 
   // Repeatly read and print entries
+  while ((pDirent = readdir(pDir)) != NULL)  
+  {
+    printf("[%s]\n", pDirent->d_name);
+  }
 
   // Close directory
+  closedir(pDir);
 
   return 0;
 }
