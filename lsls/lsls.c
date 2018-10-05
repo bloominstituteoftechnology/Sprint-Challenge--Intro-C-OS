f66de183bc19913c9c3d88b4ea9e965fe3108fed
Simon <Simon_Freelance@hotmail.com>
2018-10-05 12:49:36 -0400

    Solved challenge 2, making a cmd to list files

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -4,15 +4,28 @@
 /**
  * Main
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
   // Parse command line
 
+  if(argc > 2) {
+    puts("Please use command like so: ./lsls <directory name>");
+    return 0;
+  }
+
+  
   // Open directory
-
+  DIR *dir;
+  dir = opendir(argv[1]);
   // Repeatly read and print entries
+  struct dirent *ent;
 
+  if (dir) {
+    while((ent = readdir(dir)) != NULL) {
+      printf("%s\n", ent->d_name);
+    }
   // Close directory
-
+    closedir(dir);
+  }
   return 0;
 }
