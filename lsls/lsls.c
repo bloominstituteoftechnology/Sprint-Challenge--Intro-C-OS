078e77756933917f6ccd938b78afebf40792ef03
nchen0 <nicky.chen0@gmail.com>
2018-10-05 09:33:47 -0700

    open all file directories

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,24 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  DIR *pDir;
+  struct dirent *pDirent;
 
   // Open directory
+  pDir = opendir(".");
+  if (pDir == NULL)
+  {
+    printf("Cannot open directory %s\n", argv[1]);
+    return 1;
+  }
 
   // Repeatly read and print entries
+  while ((pDirent = readdir(pDir)) != NULL)
+  {
+    printf("[%s]\n", pDirent->d_name);
+  }
 
   // Close directory
-
+  closedir(pDir);
   return 0;
 }
