a716f4e951c41e759dc29b94aa45b15b26fbb32c
snowcoding <vimalshah77@gmail.com>
2018-10-05 08:41:36 -0700

    Basic dir open/read/close

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,30 @@
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  int len;
+  struct dirent *pDirent;
+  DIR *pDir;
 
+  // Parse command line
+  if (argc < 2) {
+      printf ("Usage: testprog <dirname>\n");
+      return 1;
+  }
+  
   // Open directory
+  pDir = opendir (argv[1]);
+  if (pDir == NULL) {
+      printf ("Cannot open directory '%s'\n", argv[1]);
+      return 1;
+  }
 
   // Repeatly read and print entries
-
+  while ((pDirent = readdir(pDir)) != NULL) {
+      printf ("%s\n", pDirent->d_name);
+  }
+  
   // Close directory
-
+  closedir (pDir);
+ 
   return 0;
 }
