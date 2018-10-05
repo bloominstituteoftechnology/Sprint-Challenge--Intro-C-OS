4dac66b046f6214fc45d22f20c4ff433706d7b15
Erin Meredith <ekmeredith@att.net>
2018-10-05 11:59:16 -0400

    completed challenge 2

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,23 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  struct dirent *ent;
+  int count;
+  for(count=0; count<argc; count++){
+    puts(argv[count]);
+  }
 
   // Open directory
-
+  DIR *dr = opendir(".");
+  if (dr == NULL) {
+    printf("Error: Could not open directory");
+    return 0;
+  }
   // Repeatly read and print entries
-
+  while((ent = readdir(dr)) != NULL){
+    printf("%s\n", ent->d_name);
+  }
   // Close directory
-
+  closedir(dr);
   return 0;
 }
