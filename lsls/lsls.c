be19ad11b8744d988158d3ea0155f546a2490938
Katie Gorbell <katiegorbell@gmail.com>
2018-10-05 11:22:12 -0500

    working on parsing command line, and opening directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,10 +6,16 @@
  */
 int main(int argc, char **argv)
 {
+  printf("argc: %d\n", argc);
   // Parse command line
-
-  // Open directory
-
+  for (int i = 0; i < argc; i++) {
+    printf("path: %s\n", argv[i]);
+    // Open directory
+    if (DIR *opendir(char *argv[i]) == NULL) {
+      fprintf(stderr, "Open directory failed\n");
+      exit(1);
+    }
+  }
   // Repeatly read and print entries
 
   // Close directory
