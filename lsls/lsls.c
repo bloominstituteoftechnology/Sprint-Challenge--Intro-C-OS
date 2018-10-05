7a62833e06bbd5c08b726e203000ea8f7995a777
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-10-05 09:59:52 -0600

    Finished the functionality to print content of current directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -9,10 +9,18 @@ int main(int argc, char **argv)
   // Parse command line
 
   // Open directory
-
+  DIR *d = opendir(".");
+  if (d == NULL) // Checks for errors
+  {
+    printf("\033[31merror\033[37m: no such file or directory\n");
+    return 0;
+  }
   // Repeatly read and print entries
-
+  struct dirent *id;
+  while ((id = readdir(d)) != NULL) {
+    printf("%s\n", id->d_name);
+  }
   // Close directory
-
+  closedir(d);
   return 0;
 }
