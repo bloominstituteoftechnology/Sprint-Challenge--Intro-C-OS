8c1c385550f4d3658b83887d69aed94c78761ed3
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-10-05 12:17:35 -0400

    added comments

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -10,29 +10,29 @@ int main(int argc, char **argv)
   DIR *pDir;
 
   // Parse command line
-  if (argc == 1) 
+  if (argc == 1) //no directory given, only ./lsls.exe was called
   {
     pDir = opendir(".");
   }
-  else if (argc == 2)   
+  else if (argc == 2)   // a directory was given, 2nd index ([1]) is the directory
   {
     pDir = opendir(argv[1]);
   }
-  else    
+  else     // incorrect usage of ./lsls.exe
   {
     printf("Usage: ./lsls.exe <directory name>\n");
     return 1;
   }
 
   // Open directory
-  if (pDir == NULL)  
+  if (pDir == NULL)  //checks if the directory is null
   {
     printf("Can't open dir %s \n", argv[1]);
     return 1;
   }
 
   // Repeatly read and print entries
-  while ((pDirent = readdir(pDir)) != NULL)  
+  while ((pDirent = readdir(pDir)) != NULL)  //until the entire directory is iterated through
   {
     printf("[%s]\n", pDirent->d_name);
   }
