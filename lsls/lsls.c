be5f999b9069539ed34f6ca495dabe85ba5a6826
M.Real <mreal126@protonmail.com>
2018-10-05 13:11:23 -0400

    about to test, added stat directory for stretch attempt.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -8,11 +11,30 @@ int main(int argc, char **argv)
 {
   // Parse command line
 
+  DIR *dir;
+  struct dirent *ent;
+  if (argc < 1) {
+      if ((dir = opendir("./")) == NULL) {
+          perror ("Cannot open ./");
+          exit (1);
+      }
+  } 
   // Open directory
+  else {
+    if ((dir = opendir (argv[1])) == NULL) {
+          perror ("Cannot open ./");
+          exit (1);
+      }
+  }
 
   // Repeatly read and print entries
 
+  while ((ent = readdir(dir)) != NULL) {
+    printf("%s\n", ent->d_name);
+  }
+
   // Close directory
+  closedir(dir);
 
   return 0;
 }
