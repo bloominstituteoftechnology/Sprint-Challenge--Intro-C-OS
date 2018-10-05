71bc396dc79af0b7176ced31607a2da6f208c94e
Marco A. Guzman <marcoguzman16@gmail.com>
2018-10-05 12:05:25 -0700

    comment

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -11,12 +11,13 @@ int main(int argc, char **argv)
   // **Parse command line**
 
   char *directname;
+  // argc==0 doesnt work.  Need to start w 1 at least(google fu) to start program.
   if (argc == 1){
     // going to set dir that im looking for to "."
     directname = ".";
   }
   else if (argc == 2){
-    directname = argv[1];
+    directname = argv[1]; // starting path .
   }
   else{
     fprintf(stderr, "ERROR: file unable to open\n");
