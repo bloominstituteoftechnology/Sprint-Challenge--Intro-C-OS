496d1f890e73c4f2c1344be342bebf72b21d9285
JacobLayton <jlayjlay3@gmail.com>
2018-10-05 10:49:10 -0700

    MVP complete - command line parser is functional

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -8,9 +8,26 @@ int main(int argc, char **argv)
 {
     // Parse command line
 
+    int i;
+    char *args;
+
+    // argc is the number of arguments entered into command line
+    // **argv is the pointer to the address of the argument string?
+    // printf("There are %d command line argument(s):\n", argc);
+    // printf("argv is : %d", argv);
+
+    for (i = 0; i < argc; i++) {
+        // printf("   %s\n", argv[i]);
+        args = argv[i];
+
+    }
+
+    // return 0;
+
+
     // Open directory
 
-    DIR *dir = opendir(".");
+    DIR *dir = opendir(args);
     struct dirent *dp;
 
     if (dir == NULL) {
