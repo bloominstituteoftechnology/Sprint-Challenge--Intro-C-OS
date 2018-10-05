5f4fb5d1242d6f0e978d93f852c5394934308ef0
Gabriel Duquette <gabrielduquette@gmail.com>
2018-10-05 13:01:10 -0400

    attempted stretch, gave up

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -29,3 +29,41 @@ int main(int argc, char **argv)
 
   return 0;
 }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <dirent.h>
+// #include <sys/stat.h>
+//
+// int main(int argc, char **argv)
+// {
+//   struct dirent *directory_struct;
+//   struct stat buf;
+//   int bytes;
+//   DIR *directory;
+//
+//   if (argc < 2) {
+//     directory = opendir(".");
+//   } else {
+//     directory = opendir(argv[1]);
+//   }
+//
+//   if (directory == NULL) {
+//     printf ("Directory '%s' can't be opened.\n", argv[1]);
+//     exit(1);
+//   }
+//
+//   directory_struct = readdir(directory);
+//   stat(asprintf(argv[1], '/', directory_struct->d_name), &buf);
+//
+//   while (directory_struct != NULL) {
+//     printf("file size is %10lld\n", buf.st_size);
+//     printf ("[%s]\n", directory_struct->d_name);
+//     directory_struct = readdir(directory);
+//     stat(asprintf(argv[1], '/', directory_struct->d_name), &buf);
+//   }
+//
+//   closedir(directory);
+//
+//   return 0;
+// }
