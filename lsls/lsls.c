6fabed9800a70319e13972dcbe2382cf5b79a2f9
unknown <may.alex43@gmail.com>
2018-10-15 12:11:24 -0400

    Adalberto May -late sprint

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -8,11 +8,29 @@ int main(int argc, char **argv)
 {
   // Parse command line
 
+  char *dirname;
+  if (argc==1) {
+    dirname= '.';
+  }
+  else if (argc == 2) {
+    dirname= argv[1];
+  }
+
   // Open directory
+
+  DIR *d = opendir(dirname);
 
   // Repeatly read and print entries
 
+  struct dirent *ent;
+
+  while((ent= readdir(d)) !=NULL) {
+    printf('%s\n', ent->d_name);
+  }
+
   // Close directory
+
+  closedir(d);
 
   return 0;
 }
