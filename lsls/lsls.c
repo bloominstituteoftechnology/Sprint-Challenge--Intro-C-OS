d7038024c47595d389ed36bc04311062d7d5fa93
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-11-09 11:22:58 -0500

    implemented printing directory from specified path

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  DIR *dir;
+  struct dirent *dp;
+  char *file_name;
 
-  // Open directory
+  if(argc != 2){
+    fprintf(stderr, "Usage: ./lsls <PATH>\n");
+    exit(1);
+  }
 
-  // Repeatly read and print entries
+  dir = opendir(argv[1]);
 
-  // Close directory
+  while((dp=readdir(dir)) != NULL){
+    printf("%s\n", dp->d_name);
+  }
 
+  closedir(dir);
   return 0;
 }
