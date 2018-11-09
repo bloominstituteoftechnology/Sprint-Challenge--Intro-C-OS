f233391a7925c9fcf19e08d335ee0f42d872b3ea
Sukhada Gholba <sukhadagholba@gmail.com>
2018-11-09 09:52:06 -0800

    completed problem-added functionality that prints out a directory listing for the directory the user specifies on the command line.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -17,19 +17,37 @@ int main(int argc, char **argv)
 	commands[0] = "ls";
  	commands[1] = "-la";
  	commands[2] = NULL;
-	char commandline[200];
 
         if(n==0){               // if no arguments are provided, print out the contents of the current directory,  
                execvp(commands[0], commands);
 	       perror("exec");
 	       exit(1); 
         }
+	else{			//if the user provides a path
+
+		 // Open directory
+		DIR *dir;
+    		struct dirent *ent;
+
+    		char buffer[50];
+
+    		strcpy(buffer, argv[1]);    //copy the path into the buffer
+    		dir = opendir(buffer);   //Open the directory
 		
-  	// Open directory
-	
-  	// Repeatly read and print entries
+		
+		if (dir == NULL) {
+            		printf ("Cannot open directory '%s'. Please provide correct path and directory name.\n", argv[1]);
+            		return 1;
+        	}
+		else{
 
-  	// Close directory
+		while((ent=readdir(dir))!=NULL)    //Repeatly read and print entries
+            	printf("%s\n", ent->d_name);
 
+    		}
+		
+    		// Close directory
+		closedir(dir);	
+	}
   return 0;
 }
