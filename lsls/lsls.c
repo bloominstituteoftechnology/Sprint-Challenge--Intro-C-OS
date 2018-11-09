eb0bb50578775d23b96e80f9ccf74759ad2e61e0
AI2EJAY <JHammett1314@gmail.com>
2018-11-09 13:21:16 -0600

    getting closer

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -8,12 +8,42 @@ Pull Request
 int main(int argc, char **argv)
 {
   // Parse command line
+    //   int i;
+    //   file_name[256]
+    // char *file_name = malloc((**argv+1) * sizeof(char));
 
-  // Open directory
+    
+    for (int i = 0; i < argc; i++) {
+        // printf("%s\n", argv[i]);
+
+    }
+
+    // printf("%s\n", name)
+
+   
+  // Open directory 
+    DIR *directory = opendir(argv[1]); 
+    struct dirent  {
+      char d_name[256];
+      ino_t d_ino; //idk if that's legit
+    };
+
+    if (directory != ".") {
+        while ((file =readdir(directory)) != NULL) {
+          printf("%s\n", file->d_name);
+          char fileName = file->d_name;
+          printf(fileName);
+          closedir(directory);
+        }
+        
+        // 
+      }
+      return 0;
+    }
+    
+    /* 
+ 
+    */
 
   // Repeatly read and print entries
-
   // Close directory
-
-  return 0;
-}
