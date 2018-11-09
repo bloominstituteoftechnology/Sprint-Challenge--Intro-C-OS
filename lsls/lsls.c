cefb82b4d10f5d344a51409c1b2d2c3743c81a3d
LaikaFusion <newrad4@gmail.com>
2018-11-09 11:28:41 -0500

    lsls working

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,28 @@
  */
 int main(int argc, char **argv)
 {
+  char * dirRequest;
   // Parse command line
-
+  if (argc == 1){
+    dirRequest = ".";
+  }
+  else{
+    dirRequest = argv[1];
+  }
   // Open directory
-
+   DIR * opendirectory =  opendir(dirRequest);
+   if (opendirectory == NULL){
+     printf("I'm afraid I can't do that");
+   }
   // Repeatly read and print entries
+     struct dirent *files;
+     files =  readdir(opendirectory);
+    while(files != NULL){
+      printf("%s\n", files->d_name);
+      files =  readdir(opendirectory);
 
+    }
   // Close directory
-
+  closedir(opendirectory);
   return 0;
 }
