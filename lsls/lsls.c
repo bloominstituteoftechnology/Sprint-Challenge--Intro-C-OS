8c5fe1ca03d2d17eee661b1587529a95178a6760
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-11-09 10:39:21 -0500

    Sprint completed

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,35 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  struct dirent *dirEnt;
+  DIR *dir;
 
   // Open directory
+  if (argc < 2)
+  {
+    dir = opendir(".");
+    while ((dirEnt = readdir(dir)) != NULL)
+    {
+      printf("[%s]\n", dirEnt->d_name);
+    }
+    closedir(dir);
+  }
+
 
   // Repeatly read and print entries
+  dir = opendir(argv[1]);
+  
 
   // Close directory
-
+  if (dir == NULL)
+  {
+    return 1;
+  }
+  while ((dirEnt = readdir(dir)) != NULL)
+  {
+    printf("[%s]\n", dirEnt->d_name);
+  }
+ 
+ closedir(dir);
   return 0;
 }
