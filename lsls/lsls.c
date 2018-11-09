0effa81a57404eca8df65f2be450744bb76f0298
Charles Cortinas <crcortinas@gmail.com>
2018-11-09 14:02:47 -0600

    mvp complete

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,29 @@
  */
 int main(int argc, char **argv)
 {
+  DIR *dir;
+  struct dirent *dp;
+  char * file_name;
+  char * directory;
+
   // Parse command line
+  if (argc < 2) {
+    directory = ".";
+  } else {
+    directory = argv[1];
+  }
 
   // Open directory
-
-  // Repeatly read and print entries
-
+  dir = opendir(directory);
+  if (dir == NULL) {
+    printf("Could not open directory\n");
+    return 1;
+  }
+  while ((dp=readdir(dir)) != NULL) {
+    file_name = dp->d_name;
+    printf("%s\n",file_name);
+  }
   // Close directory
-
+  closedir(dir);
   return 0;
 }
