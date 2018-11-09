c7e279cc1907c7beb32658ba53c4ca3551c86765
tulf <timzulf@gmail.com>
2018-11-09 11:53:58 -0500

    something prints this time. More or less there. hard to tell honestly do I really have to read the whole thing just to know what to do?

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -9,6 +9,15 @@ int main(int argc, char **argv)
   // Parse command line
 DIR *d;
 struct dirent *dir;
+if(argc == 1){
+  printf("bruh gimme somethng to work with here \n");
+} else{
+  // print out the arguments
+  printf("here they are all you had to do was speak on ye\n");
+  for(int i = 1; i<argc; i++){
+    printf("%d. %s\n", i, argv[i] );
+  }
+}
   // Open directory
   //pass in argv duh that's the current dir.
 d = opendir(argv[1]);
