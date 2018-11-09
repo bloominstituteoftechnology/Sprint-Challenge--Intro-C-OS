15ed5eed331043615286e7c542d52fb271d77587
francistse23 <wtse.ucsd@gmail.com>
2018-11-09 08:33:53 -0800

    Running file will print all files in directory.

--- examples/commandline.c
+++ examples/commandline.c
@@ -7,16 +7,34 @@
  */
 
 #include <stdio.h>
+#include <dirent.h>
+
+#define MAX_COMMANDLINE_LEN 8192
+#define MAX_COMMANDLINE_ARGS 128
+
 
 int main(int argc, char **argv)
 {
     int i;
+    struct dirent *pDirent;
+    DIR *pDir;
 
-    printf("There are %d command line argument(s):\n", argc);
-
-    for (i = 0; i < argc; i++) {
-        printf("   %s\n", argv[i]);
+    pDir = opendir(".");
+    if (pDir == NULL){
+        printf("Cannot open directory %s\n", argv[1]);
+        return 0;
     }
+    while ((pDirent = readdir(pDir)) != NULL){
+        printf("%s\n", pDirent -> d_name);
+    }
+    closedir(pDir);
+    
+
+    // printf("There are %d command line argument(s):\n", argc);
+
+    // for (i = 0; i < argc; i++) {
+    //     printf("   %s\n", argv[i]);
+    // }
 
     return 0;
 }
