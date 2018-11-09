0c0f61e9b150af40ac182a394efd310e4973e170
Carey <careycbaldwin@gmail.com>
2018-11-09 11:20:39 -0500

    another draft that allows for directory specification

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,3 +1,7 @@
+// http://www.codingalpha.com/list-all-contents-of-directory-c-program/
+// for ls program: https://www.geeksforgeeks.org/c-program-list-files-sub-directories-directory/ 
+// for readdir: http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html 
+
 #include <stdio.h>
 #include <dirent.h>
 
@@ -17,25 +21,42 @@
 //   return 0;
 // }
 
-// for ls program: https://www.geeksforgeeks.org/c-program-list-files-sub-directories-directory/ 
-// for readdir: http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html 
 
-int main(void)
-{
-    struct dirent *de; // pointer for directory entry
+// int main(void)
+// {
+//     struct dirent *de; // pointer for directory entry
 
-    //opendir() returns a point of DIR type.
-    DIR *dr = opendir(".");
+//     //opendir() returns a point of DIR type.
+//     DIR *dr = opendir(".");
 
-    if (dr == NULL) // opendir returns NULL if it can't open direcory
-    {
-        printf("Sorry, you can't open the current directory");
-        return 0;
-    }
-    while ((de = readdir(dr)) != NULL)
-        printf("%s\n", de -> d_name);
+//     if (dr == NULL) // opendir returns NULL if it can't open direcory
+//     {
+//         printf("Sorry, you can't open the current directory");
+//         return 0;
+//     }
+//     while ((de = readdir(dr)) != NULL)
+//         printf("%s\n", de -> d_name);
     
-    closedir(dr);
-    return 0;
+//     closedir(dr);
+//     return 0;
+// }
 
+// another verion try entering / at the prompt
+int main()
+{
+      char directory_name[10];
+      DIR *ptr;
+      struct dirent *directory;
+
+      printf("Enter Directory Name:\t");
+      scanf("%s", directory_name);
+      ptr = opendir(directory_name);
+      printf("\nDirectory %s\n", directory_name);
+      
+      while((directory = readdir(ptr)) != NULL)
+      {
+            printf("%s\n", directory->d_name);
+      }
+      
+      closedir(ptr);
 }
