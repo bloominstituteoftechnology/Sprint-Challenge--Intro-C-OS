7c4c073a771aa5d7b3a99927e77719ac8c90f95a
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-11-09 09:55:21 -0800

    Displaying current directory or the directory listing if directory path is specified. Working on the exit funcitonality. Receiving the following error if the path does not exists: Segmentation fault: 11

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 #include <libgen.h>
 
 /**
@@ -8,22 +9,25 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
-  // Open directory
   DIR *dir;
   struct dirent *dp;
 
-  // Repeatly read and print entries
-if ((dir = opendir(".")) == NULL ) {
-  perror("Cannot open .");
-  // exit(0);
-}
+  // Open directory
+  for (int i = 0; i < argc; i++) {
+    if (argc == 1) {
+      dir = opendir(".");
+    } else if (argc > 1) {
+      dir = opendir(argv[1]);
+    }
+  }
 
+  // Repeatly read and print entries
 while ((dp = readdir (dir)) != NULL) {
   printf("%s \n", dp->d_name);
 }
 
   // Close directory
+  closedir(dir);
 
   return 0;
 }
