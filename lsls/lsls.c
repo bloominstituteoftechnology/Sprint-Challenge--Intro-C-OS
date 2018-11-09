4293d4f64ad18592aef7031ffe91aca6066b18f8
adamcglee <adamcglee@gmail.com>
2018-11-09 09:46:30 -0800

    changed if condition to include 2 argc instead of <2. Program now works as intended and MVP is complete

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 
 /**
@@ -7,19 +8,30 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  if(argc <= 2){
   // Open directory
-  DIR *d;
-  struct dirent *dir;
-  d = opendir(".");
-  // Repeatly read and print entries
-  if (d) {
-    while ((dir = readdir(d)) != NULL) {
-      printf("%s\n", dir->d_name);
+    DIR *d;    
+    if(argv[1]){
+      d = opendir(argv[1]);
+    } else {
+      d = opendir(".");
     }
-    clodedir(d);
-  }
+    struct dirent *dir;
+  // Repeatly read and print entries
+    if(d == NULL) {
+      fprintf(stderr, "That folder does not exist!\n");
+      exit(1);
+    }else {
+      while ((dir = readdir(d)) != NULL) {
+        printf("%s\n", dir->d_name);
+      }
+    }
+    
   // Close directory
-
+    closedir(d);
+  }else {
+    fprintf(stderr, "Too many parameters, please only enter 1 filepath\n");
+    exit(1);
+  }
   return 0;
 }
