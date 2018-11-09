9482192543325cf65117ed8b8041652d80f2095f
mboegner <cmikeb@gmail.com>
2018-11-09 12:55:55 -0500

    Finished MVP. Attempting Stretch

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,33 @@
-#include <stdio.h>
-#include <dirent.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<dirent.h>
+#include<sys/types.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+    DIR *returned;
+    struct dirent *fileStruct;
 
-  // Open directory
+    char path[10];
+    if(argc == 1){
+      *path = *argv[0];
+      returned = opendir(path);
+    } else {
+      returned = opendir(argv[1]);
+    }
 
-  // Repeatly read and print entries
+    if(returned == NULL) {
+        printf("You have entered an incorrect file path.\n");
+        exit(1);
+    }
+    while((fileStruct = readdir(returned)) != NULL) {
+        printf("%s\n", fileStruct->d_name);
+    }
 
-  // Close directory
+    closedir(returned);
 
   return 0;
 }
