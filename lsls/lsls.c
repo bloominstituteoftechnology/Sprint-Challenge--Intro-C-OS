49a632d982e283669574a18c4f7d34cf95e66c6e
codejoncode <jonathanjholloway@gmail.com>
2018-11-09 12:37:51 -0600

    able to enter in . to get current directory able to enter in name to move forward also can move back with name of a directory. Couldn't get  no input to work as scanf is a infinte loop without a character

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -2,33 +2,67 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <unistd.h>
+#include <errno.h> 
+#include <sys/wait.h>
+#include <string.h>
 
-/**
- * Main
- */
-int main(int argc, char **argv)
-{
+
+// https://repl.it/@codejoncode/Sprint-Project-C-list-directory
+
+int main(int argc, char **arv) {
+  
   DIR *dir; 
   struct dirent *sd; 
+  char commandline;
+  char buff[FILENAME_MAX];
+  int i; 
 
-  
-  // Parse command line
+  while (strcmp(&commandline, "exit")!= 0)
+  {
 
-  // Open directory
-  dir = opendir(".");
-  if(dir == NULL)
-  {
-    printf("Error! Unable to open directory.\n");
-    exit(1); 
+      getcwd(buff, FILENAME_MAX); //gets the current working directory 
+      
+
+      // commandline = '.';
+
+      
+      printf("Current working dir: %s\n", buff);
+      printf("Choose a directory  . for current directory .. to go back or enter directoryName  use exit to exit \n");
+      scanf("%s", &commandline);
+      
+
+      if(commandline == 0)
+      {
+        printf("its null\n");
+      }
+
+
+
+      if (strcmp(&commandline, "exit") == 0)
+      {
+        printf("exiting"); 
+        exit(0); 
+      }
+
+      dir = opendir(&commandline);//will go forward or back. currently. 
+      if(dir == NULL)
+      {
+        printf("Unable to open directory.\n");
+        exit(1); 
+      }
+
+      readdir(dir);
+
+      // Repeatly read and print entries
+      while ( (sd=readdir(dir)) != NULL)
+      {
+        printf(">> %s\n", sd->d_name);
+      }
+      // Close directory
+      closedir(dir); 
   }
-  readdir(dir);
-  // Repeatly read and print entries
-  while ( (sd=readdir(dir)) != NULL)
-  {
-    printf(">> %s\n", sd->d_name);
-  }
-  // Close directory
-  closedir(dir); 
+
 
   return 0;
 }
