b39c43cd9f581c71bf21b7495e56aac5908e7ffc
William <kwontaewoongah@gmail.com>
2018-11-09 10:07:48 -0800

    finished stretch of get bytes

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,35 +1,50 @@
 #include <stdio.h>
 #include <dirent.h>
-
+#include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  
   struct dirent *ent;
   DIR *strm;
   if (argc==2) {
     if ((strm=opendir(argv[1]))!=NULL) {
       while ((ent=readdir(strm))!=NULL){
-      printf("%s\n", ent->d_name);
+        struct stat buf;
+        char subdir[1028] = "";
+        strcat(subdir, argv[1]);
+        if (argv[1][strlen(argv[1])-1]!='/') {
+          strcat(subdir,"/");
+        }
+        strcat(subdir, ent->d_name);
+        strcat(subdir,"\0");
+        stat(subdir, &buf);
+        printf("%lld %s\n", buf.st_size,ent->d_name);
     }
     closedir(strm);
     } else {
-    printf("Program has error.");
-    exit(-1);
+      printf("Program has error.");
+      exit(-1);
   }
-  } else if ((strm=opendir("."))!=NULL) {
-    while ((ent=readdir(strm))!=NULL){
-      printf("%s\n", ent->d_name);
+  } else if (argc==1) {
+    if ((strm=opendir("./"))!=NULL) {
+      while ((ent=readdir(strm))!=NULL){
+        struct stat buf;
+        char subdir[1028] = "";
+        strcat(subdir, "./");
+        strcat(subdir, ent->d_name);
+        strcat(subdir,"\0");
+        stat(subdir, &buf);
+        printf("%lld %s\n", buf.st_size,ent->d_name);
     }
     closedir(strm);
-  } else {
-    printf("Program has error.");
-    exit(-1);
+    } else {
+      printf("Program has error.");
+      exit(-1);
+    }
   }
-  
- 
-
   return 0;
 }
