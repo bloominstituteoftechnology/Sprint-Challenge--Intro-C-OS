09fb391e64af941ef284ae420df7012c781495b0
mkerbleski <mkerbles@gmail.com>
2018-11-09 10:22:34 -0700

    complete change directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,6 +6,34 @@
  */
 int main(int argc, char **argv)
 {
+  int i;
+  printf("%s\n", argv[1]);
+  printf("There are %d command line argument(s):\n", argc-1);
+
+  for (i = 1; i < argc; i++) {
+      printf("   %s\n", argv[i]);
+  }
+  
+  struct dirent *entries;
+  DIR *folder;
+
+  if (argc < 2) {
+      printf ("Usage: testprog <dirname>\ninclude folder to open or '.'.");
+      return 1;
+  }
+    
+  folder = opendir (argv[1]);
+  if (folder == NULL) {
+      printf ("Cannot open directory '%s'\n", argv[1]);
+      return 1;
+  }
+
+  while ((entries = readdir(folder)) != NULL) {
+      printf ("[%s]\n", entries->d_name);
+  }
+  closedir (folder);
+  return 0;
+  
   // Parse command line
 
   // Open directory
@@ -14,5 +42,4 @@ int main(int argc, char **argv)
 
   // Close directory
 
-  return 0;
 }
