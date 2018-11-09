b0aa70f7b9b4929481e1ecfe48e70b62a46b387d
vanceleon <vanceleon44@gmail.com>
2018-11-09 17:52:18 -0500

    latest

---
+++ lsls/lsls-old.c
@@ -0,0 +1,122 @@
+
+//   while (printf(">   "), fgets(commandLine, sizeof commandLine, stdin) != NULL) {
+
+
+//       if((dir = opendir(".")) == NULL) {
+//         perror("Cannot open");
+//         exit(1);
+//       }
+//       while ((dp = readdir(dir)) != NULL) {
+//         if( errno != 0) {
+//           perror("Error reading directory");
+//         }else{
+//           (void)
+//         }
+//       }
+      
+      
+//     //   int pid = fork();
+//     //   if (pid < 0 ) {
+//     //     fprintf(stderr, "Fork Failed \n");
+//     //     exit(1);
+//     //   } else if (pid == 0) {
+//     //     execvp(exec_commands[0], exec_commands);
+//     //     perror("exec");
+//     //     exit(1);
+//     //   } else {
+//     //     wait(NULL);
+//     //     // printf("Parent Process");
+
+//     //   }
+    
+
+
+
+
+//     }
+
+  
+//   return 0;
+// }
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int main(int argc, char **argv)
+// {
+//   // char path = ;
+//     int d = opendir("/mnt/c/Users/VLeon/Documents/Code");
+//     printf("d: %s", d);
+//     while(d != "\0" || NULL ){
+//       printf("while loop d: %s", d);
+//       readdir(d);
+
+//     };
+
+//   // if ((dir = opendir(".")) == NULL) {
+//   //   perror("Cannot open .");
+//   //   exit(1);
+//   // }
+
+//   // while ((dp = readdir(dir)) != NULL) {
+
+//   // }
+  
+
+//   return 0;
+// }
+
+
+
+
+// int main(int argc, char **argv)
+// {
+//   char commandLine[9892];
+
+  
+//   if (pipe(p) < 0) {
+//     fprintf(stderr, "fork failed\n");
+//     exit(1);
+
+
+//   int pid = fork();
+//   }else if (pid == 0) {
+//     // open
+
+//     // read and print
+
+
+
+//     // printf("Child Process");
+//   }else {
+//     wait(NULL);
+//     // printf("Parent Process");
+
+//   }
+
+//   return 0;
+// }
+
+
+
--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,8 +6,8 @@
 #include <errno.h>
 #include <sys/wait.h>
 
-#define MAX_CMD_LEN 9824
-#define MAX_CMD_ARGS 128
+// #define MAX_CMD_LEN 9824
+// #define MAX_CMD_ARGS 128
 
 /**
  * Main
@@ -23,163 +23,51 @@
 
 
 
-void parse_commandline(char *commandline, char *exec_commands[], int max_count)
+int main(int argc, char **argv)
+// int main(void)
 {
-  char *separators = "\r\n\t";
-  char *token;
-  char *str = commandline;
-  int i = 0;
+  char *dirname;
+  // char commandLine[MAX_CMD_LEN];
 
-   while ((token = strtok(str, separators)) != NULL && i < max_count){
-     str = NULL;
-     exec_commands[i++] = token;
-   }
-
-  exec_commands[i] = NULL;
-}
-
-
-// int main(int argc, char **argv)
-int main(void)
-{
-  char commandLine[MAX_CMD_LEN];
-  char *exec_commands[MAX_CMD_ARGS];
-
-// while (commandLine == NULL ){
-//   if (commandLine == NULL) {
-//       char defaultCMD = "pwd";
-//       printf("testing Null");
-//     exec_commands[0] = defaultCMD;
-//     printf(exec_commands[0]);
-//     DIR *opendir(const char *defaultCMD);
-
-//   }
-
-// }
-
-  while (printf("> "), fgets(commandLine, sizeof commandLine, stdin) != NULL) {
-    parse_commandline(commandLine, exec_commands,MAX_CMD_ARGS-1);
-
-    // if (strcmp(exec_commands[0], "exit") == 0) {
-    //   exit(0);
-    // }else {
-
-    // }
-      DIR * dir;
-      struct dirent *dp;
+  if (argc == 1){
+    dirname = ".";
+  }else if (argc == 2) {
+    dirname = argv[1];
+  }else{
+    fprintf(stderr, "error: lsls [dirname]\n");
+    return 1;
+  }
 
 
-      if((dir = opendir(".")) == NULL) {
-        perror("Cannot open");
-        exit(1);
-      }
-      while ((dp = readdir(dir)) != NULL) {
-
-      }
-      
-      
-    //   int pid = fork();
-    //   if (pid < 0 ) {
-    //     fprintf(stderr, "Fork Failed \n");
-    //     exit(1);
-    //   } else if (pid == 0) {
-    //     execvp(exec_commands[0], exec_commands);
-    //     perror("exec");
-    //     exit(1);
-    //   } else {
-    //     wait(NULL);
-    //     // printf("Parent Process");
-
-    //   }
+  // while (printf("> ", fgets(commandLine, sizeof commandLine, stdin)) != NULL){
     
+    DIR *d = opendir(dirname);
+
+    if (d == NULL ) {
+      perror("lsls");
+      return 1;
+    }
+
+    struct dirent *de;
+    char fullpath[9812];
 
 
+    while((de = readdir(de)) != NULL ){
+      sprintf(fullpath, "%s/%s", dirname, de->d_name);
+      struct stat buf;
 
+    if (stat(fullpath, &buf) == -1) {
+      perror("stat")
+    }
+
+      printf("%s  %d\n", de->d_name);
 
     }
 
-  
+    int closedir(DIR *de);
+
+  // }
   return 0;
+
+
 }
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// int main(int argc, char **argv)
-// {
-//   // char path = ;
-//     int d = opendir("/mnt/c/Users/VLeon/Documents/Code");
-//     printf("d: %s", d);
-//     while(d != "\0" || NULL ){
-//       printf("while loop d: %s", d);
-//       readdir(d);
-
-//     };
-
-//   // if ((dir = opendir(".")) == NULL) {
-//   //   perror("Cannot open .");
-//   //   exit(1);
-//   // }
-
-//   // while ((dp = readdir(dir)) != NULL) {
-
-//   // }
-  
-
-//   return 0;
-// }
-
-
-
-
-// int main(int argc, char **argv)
-// {
-//   char commandLine[9892];
-
-  
-//   if (pipe(p) < 0) {
-//     fprintf(stderr, "fork failed\n");
-//     exit(1);
-
-
-//   int pid = fork();
-//   }else if (pid == 0) {
-//     // open
-
-//     // read and print
-
-
-
-//     // printf("Child Process");
-//   }else {
-//     wait(NULL);
-//     // printf("Parent Process");
-
-//   }
-
-//   return 0;
-// }
-
-
-
