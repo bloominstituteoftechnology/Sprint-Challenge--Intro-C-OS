f37b02053baef45a58840f6c151544aaa28916f1
rverdi642 <rverdi642@gmail.com>
2018-11-09 12:24:12 -0600

    MVP finished I believe...not much time left but will work on stretch for listing size

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -4,8 +4,6 @@
 /**
  * Main
  */
-int main(int argc, char **argv)
-{
   int main(int argc, char **argv)
 {
   // Parse command line
@@ -18,10 +16,33 @@ int main(int argc, char **argv)
   }
 
   // Open directory
+  /*The opendir() function opens a directory stream corresponding to the directory name, 
+  and returns a pointer to the directory stream. 
+  The stream is positioned at the first entry in the directory.*/
+
+ if (argv[1] == NULL)
+  {
+    dir = opendir(".");
+  }
+  else
+  {
+    dir = opendir(argv[1]);
+  }
+
+  if (dir == NULL)
+  {
+    printf("Cannot open directory %s\n", argv[1]);
+    return 1;
+  }
 
   // Repeatly read and print entries
+  while ((pDirent = readdir(dir)) != NULL)
+  {
+    printf("%s\n", pDirent->d_name);
+  }
 
   // Close directory
-
+  closedir(dir);
   return 0;
 }
+
