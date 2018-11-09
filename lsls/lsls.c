08cd587575609a1082aed9e1c41d9adb9ff9fe8a
Arthur Bates <aabatesj@gmail.com>
2018-11-09 13:05:43 -0500

    .lsls.c
    printing only root directory so far

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,25 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  DIR *dir;
+  struct dirent *dp;
+  char *filename = ".";
   // Open directory
-
+  if ((dir = opendir(filename)) == NULL)
+  {
+    perror("Can't open this file");
+  }
   // Repeatly read and print entries
 
   // Close directory
+  else
+  {
+    while ((dp = readdir(dir)) != NULL)
+    {
+      printf(" %s\n", dp->d_name);
+    }
+    closedir(dir);
+  }
 
   return 0;
 }
