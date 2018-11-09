e24d25e45510cc4702dc4f2c33d4a1887598083b
AI2EJAY <JHammett1314@gmail.com>
2018-11-09 13:55:53 -0600

    MVP

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -8,42 +8,35 @@ Pull Request
 int main(int argc, char **argv)
 {
   // Parse command line
-    //   int i;
-    //   file_name[256]
-    // char *file_name = malloc((**argv+1) * sizeof(char));
-
-    
-    for (int i = 0; i < argc; i++) {
-        // printf("%s\n", argv[i]);
-
-    }
-
+    // for (int i = 0; i < argc; i++) {
+  // printf("%s\n", argv[i]);
+    // }
     // printf("%s\n", name)
 
-   
   // Open directory 
     DIR *directory = opendir(argv[1]); 
-    struct dirent  {
-      char d_name[256];
-      ino_t d_ino; //idk if that's legit
-    };
-
-    if (directory != ".") {
-        while ((file =readdir(directory)) != NULL) {
+    // struct dirent *file; {
+    //   char d_name[256];
+    //   __ino_t d_ino; //idk if that's legit oooh i see. you don't need to do this, because this struct is declared in dirent.h
+    // };
+    struct dirent *file; 
+    if (directory != NULL) {
+        while((file = readdir(directory)) != NULL)
+        {
+  // Repeatly read and print entries
           printf("%s\n", file->d_name);
-          char fileName = file->d_name;
-          printf(fileName);
-          closedir(directory);
+          // char fileName = file->d_name;
+          // printf(fileName);
         }
         
+          // Close directory
+          closedir(directory); 
+  }  
         // 
-      }
       return 0;
-    }
-    
+}
+  
     /* 
  
     */
 
-  // Repeatly read and print entries
-  // Close directory
