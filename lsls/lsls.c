1be72e01f6ffe55f8ff9dd01146c3195ed43b2b7
skennedy93 <sean47900@gmail.com>
2018-11-09 12:17:01 -0600

    restarting using code given in commandline.c

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,25 +1,15 @@
 #include <stdio.h>
 #include <dirent.h>
-#include <stdlib.h>
+
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-    char *dirname;
   // Parse command line
-  if(argc == 1)dirname = ".";
-  else if(argc == 2) dirname = argv[1];
-  else {
-    fprintf(stderr, "usage: lsls [dir]\n");
-    exit(1);
-  }
+
   // Open directory
-  DIR *d = opendir(dirname);
-   if (d == NULL){
-    fprintf(stderr, "lsls:Can't find %s \n", dirname);
-    exit(2);
-  }
+
   // Repeatly read and print entries
 
   // Close directory
