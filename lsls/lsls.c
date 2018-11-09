4c75e5aa15f470f352a01dcc4af3df859061f7ba
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-11-09 08:41:28 -0800

    implement lsls

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  struct dirent *pDirent;
+  DIR *pDir;
   // Parse command line
+  if (argc < 2) {
+    printf("Usage: ./lsls <directory>");
+    return 1;
+  }
 
   // Open directory
+  pDir = opendir(argv[1]);
+  if (pDir == NULL) {
+    printf("Cannot open directory '%s'\n", argv[1]);
+    return 1;
+  }
 
   // Repeatly read and print entries
 
-  // Close directory
+  while ((pDirent = readdir(pDir)) != NULL) {
+    printf("%s\n", pDirent->d_name);
+  }
 
+  // Close directory
+  closedir(pDir);
   return 0;
 }
