85272761b47a5b942744dd790abf9ad857b120e0
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-11-09 10:56:30 -0600

    Added to Challenge
    Currently working on the repeatrly read and print entries

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <libgen.h>
+#include <sys/types.h>
 
 /**
  * Main
@@ -15,12 +17,16 @@ int main(int argc, char **argv)
     printf("   %s\n", argv[i]);
   }
   // Open directory
-  DIR *d = opendir(".");
-  printf("Testing %s\n", d == NULL ? "FAIL": "PASS");
-  closedir(d);
+  DIR *d;
+  struct dirent *dp;
   // Repeatly read and print entries
-
+  if((d = opendir("."))==NULL){
+    perror("Cannot open .");
+  }
+  while ((dp = readdir (d)) != NULL){
+    printf("  %s\n", argv[i]);
+  }
   // Close directory
-
+  closedir(d);
   return 0;
 }
