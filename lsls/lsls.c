93124a420a2362b3b425387dfe96716db52d54ce
DasGMA <dasgrigoma@gmail.com>
2018-11-09 08:26:54 -0800

    Printing root directory.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,23 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  DIR *directory;
+  struct dirent *entry;
+  char *currentDirectory = ".";
 
   // Open directory
+  if ((directory = opendir(currentDirectory)) == NULL) {
+    perror("The Directory can not be opened.");
+  }
 
   // Repeatly read and print entries
-
+  else {
+    while ((entry = readdir(directory)) != NULL) {
+      printf("%s\n", entry->d_name);
+    }
   // Close directory
-
+    closedir(directory);
+  }
+  
   return 0;
 }
