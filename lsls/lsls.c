3eeda46a4b9153413cbd44e23f03ac6ec402505d
Carey <careycbaldwin@gmail.com>
2018-11-09 11:04:40 -0500

    first draft of lsls

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
 #include <dirent.h>
 
-/**
- * Main
- */
-int main(int argc, char **argv)
+// /**
+//  * Main
+//  */
+// int main(int argc, char **argv)
+// {
+//   // Parse command line
+
+//   // Open directory
+
+//   // Repeatly read and print entries
+
+//   // Close directory
+
+//   return 0;
+// }
+
+// for ls program: https://www.geeksforgeeks.org/c-program-list-files-sub-directories-directory/ 
+// for readdir: http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html 
+
+int main(void)
 {
-  // Parse command line
+    struct dirent *de; // pointer for directory entry
 
-  // Open directory
+    //opendir() returns a point of DIR type.
+    DIR *dr = opendir(".");
 
-  // Repeatly read and print entries
+    if (dr == NULL) // opendir returns NULL if it can't open direcory
+    {
+        printf("Sorry, you can't open the current directory");
+        return 0;
+    }
+    while ((de = readdir(dr)) != NULL)
+        printf("%s\n", de -> d_name);
+    
+    closedir(dr);
+    return 0;
 
-  // Close directory
-
-  return 0;
 }
