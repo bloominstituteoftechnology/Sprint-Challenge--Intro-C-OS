8bcdf1675072ab323e3c988c4f32674649b6338e
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-11-09 10:24:52 -0600

    Working on challenge 2

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,9 +7,17 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-  
-  // Open directory
+  int i;
 
+  printf("%d Command line argument(s):\n", argc);
+
+  for(i = 0; i < argc; i++){
+    printf("   %s\n", argv[i]);
+  }
+  // Open directory
+  DIR *d = opendir(".");
+  printf("Testing %s\n", d == NULL ? "FAIL": "PASS");
+  closedir(d);
   // Repeatly read and print entries
 
   // Close directory
