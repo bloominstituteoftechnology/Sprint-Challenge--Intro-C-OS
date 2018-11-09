1f82aca43c6aaae1cf1d923e4852d41abace5d0d
mkerbleski <mkerbles@gmail.com>
2018-11-09 11:53:19 -0700

    not much more completed but tired to add size

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  int i;
+  // int i;
   // printf("%i\n", argc);
   // printf("%s\n", argv[0]);
   // printf("There are %d command line argument(s):\n", argc-1);
@@ -19,7 +20,6 @@ int main(int argc, char **argv)
   DIR *folder;
 
   if (argc < 2) {
-      // printf ("Usage: testprog <dirname>\ninclude folder to open or '.'.");
       folder = opendir (".");
       while ((entries = readdir(folder)) != NULL) {
           printf ("[%s]\n", entries->d_name);
@@ -30,21 +30,20 @@ int main(int argc, char **argv)
   folder = opendir (argv[1]);
   if (folder == NULL) {
       printf ("Cannot open directory '%s'\n", argv[1]);
+      // exit();
       return 1;
   }
 
   while ((entries = readdir(folder)) != NULL) {
-      printf ("[%s]\n", entries->d_name);
+      printf ("[%s]\n", entries->d_name);//rough equivilent of dot notation essentially
+      int stat(char * argv, struct stat *buf);
+      struct stat buf;
+
+      stat("./lsls.c", &buf);
+
+      printf("file size is %10lld\n", buf.st_size);
   }
   closedir (folder);
   return 0;
-  
-  // Parse command line
-
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
 
 }
