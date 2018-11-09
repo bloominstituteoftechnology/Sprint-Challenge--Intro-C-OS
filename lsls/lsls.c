ceb842b24f1694f120d102036ee99334559dbd12
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-11-09 11:36:04 -0500

    added marking directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -22,7 +22,11 @@ int main(int argc, char **argv)
 
   while((dp=readdir(dir)) != NULL){
     stat(dp->d_name, &buf);
-    printf("%10lld %s\n", buf.st_size, dp->d_name);
+    if((buf.st_mode&S_IFDIR) != 0){
+      printf("     <DIR> %s\n", dp->d_name);
+    }else{
+      printf("%10lld %s\n", buf.st_size, dp->d_name);
+    }
   }
 
   closedir(dir);
