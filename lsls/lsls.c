a6efcea363befcd78ead293397dbbe70b8eed3d9
Alberto-Funk <laptopreneur@gmail.com>
2018-11-09 08:40:00 -0800

    WIP

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,48 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  struct dirent *ent;
+  DIR *directory;
 
-  // Open directory
+  if (argc == 1)
+  {
+    directory = opendir(".");
+  }
+  
+  if (argc == 2)
+  {
+    directory = opendir(argv[1]);
+  }
 
-  // Repeatly read and print entries
+  if (directory == NULL) 
+  {
+    printf("Cannot open directory.");
+    exit(0);
+  }
 
-  // Close directory
+  ent = readdir(directory);
+  // pid_t pid = fork();
 
+  // if (pid == 0) {
+  //   printf("%s\n", ent->d_name);
+  //   exit(1);
+  // } else {
+  //   wait(NULL);
+  // }
+
+  while (ent != NULL)
+  {
+    printf("%scheck\n", ent->d_name);
+  }
+
+
+  closedir(directory);
   return 0;
 }
