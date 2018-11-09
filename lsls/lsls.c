0efe7427721be39fda893cf83f0f800b7b63b57b
tulf <timzulf@gmail.com>
2018-11-09 11:29:40 -0500

    segementation fault

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -10,7 +10,8 @@ int main(int argc, char **argv)
 DIR *d;
 struct dirent *dir;
   // Open directory
-d = opendir(".");
+  //pass in argv duh that's the current dir.
+d = opendir(argv[1]);
 if(d){
     // Repeatly read and print entries
   while((dir = readdir(d)) != NULL){
