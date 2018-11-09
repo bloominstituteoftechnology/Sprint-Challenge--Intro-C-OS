bb1a5cea07d0f2f9a5eefb36f395c8f2c9388de1
Sukhada Gholba <sukhadagholba@gmail.com>
2018-11-09 08:49:54 -0800

    added functionality to check if no arguments are passed then print out the contents of the current directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
-
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  	// Parse command line
+	int n;
+        n = argc-1;
+	char *command="ls";
 
-  // Open directory
+        if(n==0){               // if no arguments are provided, print out the contents of the current directory,  
+               execlp(command, command, NULL);
+	       perror("exec");
+	       exit(1); 
+        }
 
-  // Repeatly read and print entries
+  	// Open directory
 
-  // Close directory
+  	// Repeatly read and print entries
+
+  	// Close directory
 
   return 0;
 }
