fd0db715818aa97c48082af855a42f8bb4d1d9cf
adamcglee <adamcglee@gmail.com>
2018-11-09 08:29:35 -0800

    Adam Lee: Sprint-Challenge--Intro-C-OS
    answered question and started working on code to print current directory.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -9,9 +9,16 @@ int main(int argc, char **argv)
   // Parse command line
 
   // Open directory
-
+  DIR *d;
+  struct dirent *dir;
+  d = opendir(".");
   // Repeatly read and print entries
-
+  if (d) {
+    while ((dir = readdir(d)) != NULL) {
+      printf("%s\n", dir->d_name);
+    }
+    clodedir(d);
+  }
   // Close directory
 
   return 0;
