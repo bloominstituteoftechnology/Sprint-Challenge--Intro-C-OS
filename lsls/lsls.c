e37784c1fc5e7dbd7faee99599c9d33bcfd7a075
Charlie Winslow <cwinslow22@yahoo.com>
2018-11-09 12:59:48 -0600

    commented out unsuccessful implmentation of 1st stretch

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,36 +1,51 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
-
-
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
   struct dirent *de;  // Pointer for directory entry 
 
-  // opendir() returns a pointer of DIR type.  
+  // parsing from the command line
   char* parse = argv[1];
+  // if (argc > 1) {
+  //   char* parse = argv[1];
+  // } else char* parse = ".";
   
+  
+  // opendir() returns a pointer of DIR type.  
   DIR *dr = opendir(parse); 
+  // if (argc < 2) {
+  //   printf("argc is less than 2\n");
+  //   DIR *dr = opendir(".");
+  // }
+  // else if (argc >= 2) {
+  //   printf("argc is 2 or greater\n");
+  //   DIR *dr = opendir(parse);
+  // }
 
   if (dr == NULL)  // opendir returns NULL if couldn't open directory 
   { 
       printf("Could not open directory %s\n", parse ); 
       return 0; 
   } 
+  struct stat buf;
 
   while ((de = readdir(dr)) != NULL) 
-          printf("%s\n", de->d_name); 
+          // stat(de->d_name, &buf);
+          // printf("%10ld %s\n", buf.st_size, de->d_name); 
+          printf("%s\n", de->d_name);
 
   // printf("%s\n", parse);
+  
+
+  // stat("./lsls.c", &buf);
+
+  // printf("file size is %lld\n", buf.st_size);
+
   printf("---end of directory--- \n");
   closedir(dr);  
 
